12051ca33d2b583d173b8aecd67c21c28a773568
Jibin Bao <jbao@nvidia.com>
2021-04-27 13:25:24 +0800

    add common api for app extension
    Change-Id: Ic19182c6276e5db04832a8c3caeffba66b759921

--- ngts/cli_wrappers/common/general_clis_common.py
+++ ngts/cli_wrappers/common/general_clis_common.py
@@ -1,4 +1,7 @@
+import logging
 from ngts.cli_wrappers.interfaces.interface_general_clis import GeneralCliInterface
+
+logger = logging.getLogger()
 
 
 class GeneralCliCommon(GeneralCliInterface):
@@ -17,3 +20,22 @@ class GeneralCliCommon(GeneralCliInterface):
     def stop_service(engine, service):
         output = engine.run_cmd('sudo service {} stop'.format(service), validate=True)
         return output
+
+    @staticmethod
+    def get_container_status(engine, app_name):
+        """
+        Get specified container's status:
+        Example:
+            docker ps -a -f name=snmp$ --format "{'ID':'{{ .ID }}', 'Names':'{{ .Names }}', 'Status':'{{ .Status }}'}"
+            {'ID':'bb2ef5fcd2b1', 'Names':'snmp', 'Status':'Up 3 hours'}
+        :param engine: ssh engine object
+        :param app_name: ssh engine object
+        :Return app container status, None if no container data
+        """
+        container_data_format = "{'ID':'{{ .ID }}', 'Names':'{{ .Names }}', 'Status':'{{ .Status }}'}"
+        app_container_info = engine.run_cmd('docker ps -a -f name={}$ --format "{}" '.format(app_name, container_data_format), validate=True)
+        logger.info("get {} container status:{}".format(app_name, app_container_info))
+        app_container_status = None
+        if app_container_info:
+            app_container_status = eval(app_container_info)["Status"]
+        return app_container_status
--- ngts/cli_wrappers/sonic/sonic_app_extension_clis.py
+++ ngts/cli_wrappers/sonic/sonic_app_extension_clis.py
@@ -1,4 +1,10 @@
+import logging
+
 from ngts.cli_wrappers.common.general_clis_common import GeneralCliCommon
+from ngts.cli_util.cli_parsers import generic_sonic_output_parser
+
+
+logger = logging.getLogger()
 
 
 class SonicAppExtensionCli:
@@ -9,9 +15,11 @@ class SonicAppExtensionCli:
         pass
 
     @staticmethod
-    def add_repository(engine, app_name, repository_name):
-        engine.run_cmd('sudo sonic-package-manager repository add {} {}'.format(app_name, repository_name),
-                       validate=True)
+    def add_repository(engine, app_name, repository_name, version=None):
+        if version:
+            engine.run_cmd('sudo sonic-package-manager repository add {} {} --default-reference={}'.format(app_name, repository_name, version), validate=True)
+        else:
+            engine.run_cmd('sudo sonic-package-manager repository add {} {}'.format(app_name, repository_name), validate=True)
 
     @staticmethod
     def remove_repository(engine, app_name):
@@ -25,9 +33,76 @@ class SonicAppExtensionCli:
             engine.run_cmd('sudo sonic-package-manager install {} -y'.format(app_name), validate=True)
 
     @staticmethod
-    def uninstall_app(engine, app_name):
-        engine.run_cmd('sudo sonic-package-manager uninstall {} -y'.format(app_name), validate=True)
+    def uninstall_app(engine, app_name, is_force=True):
+        if is_force:
+            engine.run_cmd('sudo sonic-package-manager uninstall {} -y --force'.format(app_name), validate=True)
+        else:
+            engine.run_cmd('sudo sonic-package-manager uninstall {} -y'.format(app_name), validate=True)
 
     @staticmethod
     def show_app_list(engine):
         return engine.run_cmd('sudo sonic-package-manager list')
+
+    @staticmethod
+    def parse_app_package_list_dict(engine):
+        """
+        Parse app package data into dict by "sonic-package-manager list" output
+        :param engine: ssh engine object
+        :Return app package dict, or raise exception
+        """
+        app_package_repo_list = SonicAppExtensionCli.show_app_list(engine)
+        app_package_repo_dict = generic_sonic_output_parser(app_package_repo_list,
+                                                            headers_ofset=0,
+                                                            len_ofset=1,
+                                                            data_ofset_from_start=2,
+                                                            data_ofset_from_end=None,
+                                                            column_ofset=2,
+                                                            output_key='Name')
+        return app_package_repo_dict
+
+    @staticmethod
+    def uninstall_app(engine, app_name, is_force=False):
+        if is_force:
+            engine.run_cmd('sudo sonic-package-manager uninstall --force {} -y'.format(app_name), validate=True)
+        else:
+            engine.run_cmd('sudo sonic-package-manager uninstall {} -y'.format(app_name), validate=True)
+
+    @staticmethod
+    def enable_app(engine, app_name):
+        engine.run_cmd('sudo config feature state {} enabled'.format(app_name), validate=True)
+
+    @staticmethod
+    def disable_app(engine, app_name):
+        engine.run_cmd('sudo config feature state {} disabled'.format(app_name), validate=True)
+
+    @staticmethod
+    def install_app_from_tarball(engine, tarball_name):
+        engine.run_cmd("sudo spm install -y --from-tarball {}".format(tarball_name), validate=True)
+
+    @staticmethod
+    def upgrade_app(engine, app_name: str, version: str, is_force_upgrade: bool = False, validate: bool = True) -> str:
+        """
+        Upgrade app with specified version from repo:
+        :param engine: ssh engine object
+        :param app_name: app name
+        :param version:  app package version
+        Return output from command
+        """
+        if is_force_upgrade:
+            output = engine.run_cmd("sudo sudo spm upgrade -y -f {}={}".format(app_name, version), validate=validate)
+        else:
+            output = engine.run_cmd("sudo sudo spm upgrade -y {}={}".format(app_name, version), validate=validate)
+
+        return output
+
+    @staticmethod
+    def upgrade_app_from_tarbll(engine, tarball_name: str) -> None:
+        """
+        Upgrade app with specified version from tarball:
+        :param engine: ssh engine object
+        :param tarball_name: app name
+        :parm validate: bool
+        """
+        engine.run_cmd("sudo spm upgrade -y --from-tarball {}".format(tarball_name))
+
+
