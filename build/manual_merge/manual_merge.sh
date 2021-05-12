c5c03b91315110c17de1d01de96fa790bde174bb
Asaf Lavi <asafl@mellanox.com>
2021-05-12 21:47:26 +0300

    Added manual merge script and job
    Change-Id: I886d2c34a23b9659283d736461a4459c4fcc5741
    Signed-off-by: Asaf Lavi <asafl@mellanox.com>

---
+++ build/manual_merge/manual_merge.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+echo
+echo "*********************************************************************************"
+echo "Welcome to the manual merge."
+echo "This script simulates the job sonic_mgmt_merge until the 'solve conflicts' part."
+echo "It will create a new folder for you to solve the conflicts on."
+echo "format: ./manual_merge.sh <local branch> <github branch>"
+echo "*********************************************************************************"
+echo
+
+if [ $# -lt 2 ]
+  then
+    echo "ERROR - Please give the name of the local branch and the github branch you want to run on."
+    echo "        Format: ./manual_merge.sh <local branch> <github branch>"
+    echo "        Example: ./manual_merge.sh develop master"
+    exit 1
+fi
+
+
+tmp_dir="sonic-mgmt_$(date '+%Y_%m_%d-%H_%M_%S')"
+GITHUB_REPOSITORY=https://github.com/Azure/sonic-mgmt.git
+LOCAL_BRANCH=$1
+GITHUB_BRANCH=$2
+echo "LOCAL_BRANCH=${LOCAL_BRANCH}"
+echo "GITHUB_BRANCH=${GITHUB_BRANCH}"
+
+echo "Cloning sonic-mgmt (branch ${LOCAL_BRANCH}) into $(tmp_dir)"
+git clone -b ${LOCAL_BRANCH} "ssh://10.7.77.140:29418/switchx/sonic/sonic-mgmt" -b ${LOCAL_BRANCH} ${tmp_dir}
+cd ${tmp_dir} || exit 1
+FILE=".git/hooks/commit-msg"
+if [[ ! -f "$FILE" ]]; then
+    echo "$FILE does not exists. copying commit message"
+    scp -p -P 29418 10.7.77.140:hooks/commit-msg ".git/hooks/"
+fi
+echo "merging upstream/${GITHUB_BRANCH}"
+git remote add upstream ${GITHUB_REPOSITORY} && git fetch upstream
+git merge upstream/${GITHUB_BRANCH}
+
+echo
+echo "*********************************************************************************"
+echo "script passed well. your folder is ready here: ${tmp_dir}"
+echo "1. cd ${tmp_dir}"
+echo "2. Fix the conflicts."
+echo "3. Commit to gerrit:"
+echo "   git commit -m 'Merge remote-tracking branch 'upstream/${GITHUB_BRANCH}' into ${LOCAL_BRANCH}'"
+echo "4. git review"
+echo "5. Trigger the jenkins job http://fit81.mtl.labs.mlnx:8080/job/sonic_mgmt_merge_trigger_ci/"
+echo "*********************************************************************************"
+echo
--- build/merge/push_and_code_review.groovy
+++ build/merge/push_and_code_review.groovy
@@ -18,31 +18,44 @@ def run_step(name, ci_tools)
 {
     try
     {
-        //get git branch
-        def git_branch = ci_tools.run_sh_return_output("git rev-parse --abbrev-ref HEAD").trim()
+        def change_number_list= []
 
-        //only trigger formal
-        if (env.NEW_CHANGES) {
-            //push to gerrit and get commits url's
-            print "Pushing commit/s to gerrit"
-            def push_output = ci_tools.run_sh_return_output("git push origin ${git_branch}:refs/for/${git_branch}")
+        if (ci_tools.is_parameter_contains_value(env.SHA1_MERGE_COMMIT)){
+            def related_changes = GerritTools.query_gerrit(ci_tools, 'GET', "/changes/${env.SHA1_MERGE_COMMIT}/revisions/${env.SHA1_MERGE_COMMIT}/related", [])
+            for (change in related_changes["changes"]){
+                change_number_list.add(change["_change_number"])
+            }
+            //move merge change number to the end
+            def merge_change_number = change_number_list[0]
+            change_number_list = change_number_list.drop(1) + merge_change_number
+
+        } else {
+            //get git branch
+            def git_branch = ci_tools.run_sh_return_output("git rev-parse --abbrev-ref HEAD").trim()
+
+            //only trigger formal
+            if (env.NEW_CHANGES) {
+                //push to gerrit and get commits url's
+                print "Pushing commit/s to gerrit"
+                def push_output = ci_tools.run_sh_return_output("git push origin ${git_branch}:refs/for/${git_branch}")
 
 
-            //loop over push string for getting gerrit url's
-            def change_number_list = []
-            def change_number
-            push_output.readLines().each { def line ->
-                if (line.contains("http")) {
-                    line.split(" ").each { split_str ->
-                        if (split_str.contains("http")) {
-                            int i = split_str.lastIndexOf("/")
-                            change_number = split_str.substring(i).substring(1)
-                            change_number_list.add(change_number)
+                //loop over push string for getting gerrit url's
+                def change_number
+                push_output.readLines().each { def line ->
+                    if (line.contains("http")) {
+                        line.split(" ").each { split_str ->
+                            if (split_str.contains("http")) {
+                                int i = split_str.lastIndexOf("/")
+                                change_number = split_str.substring(i).substring(1)
+                                change_number_list.add(change_number)
+                            }
                         }
                     }
-
                 }
             }
+        }
+        if (ci_tools.is_parameter_contains_value(env.SHA1_MERGE_COMMIT) || env.NEW_CHANGES) {
             env.MAIL_SUBJECT = "New changes pushed to Gerrit: ${env.GERRIT_HTTP_URL}/${change_number_list.last()}"
             currentBuild.description = env.MAIL_SUBJECT
 
@@ -67,7 +80,7 @@ def run_step(name, ci_tools)
             //add build name and number
             def topic = "${JOB_BASE_NAME} #${BUILD_NUMBER}"
 
-            def possible_topic_list = ["RUN_COMMUNITY_REGRESSION", "IMAGE_VERSION", "IMAGE_BRANCH"]
+            def possible_topic_list = ["RUN_COMMUNITY_REGRESSION", "IMAGE_VERSION", "IMAGE_GITHUB_BRANCH"]
 
             possible_topic_list.each { possible_topic ->
                 if (param_contain_value(ci_tools, env."${possible_topic}")) {
