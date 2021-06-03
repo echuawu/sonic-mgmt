da097985a2904c46bb50a00d6db78ac25faff8c6
Jibin Bao <jbao@nvidia.com>
2021-06-03 17:55:24 +0800

    change for app statu check not regardless of upgrade
    Change-Id: I91c4b790af858bf19bdaa40658a2eda0895534d9

---
+++ ngts/tests/push_build_tests/app_extension/test_app_ext_status.py
@@ -0,0 +1,20 @@
+import pytest
+import allure
+
+from ngts.tests.nightly.app_extension.app_extension_helper import verify_app_container_up_and_repo_status_installed
+
+
+@pytest.mark.app_ext
+@pytest.mark.build
+@pytest.mark.push_gate
+def test_app_status(engines, pre_app_ext):
+    """
+    This test validates app container status is up and app is installed
+    """
+    is_support_app_ext, app_name, version, _ = pre_app_ext
+
+    if not is_support_app_ext:
+        pytest.skip('This build not support app ext')
+
+    with allure.step("Verify app up and status is installed"):
+        verify_app_container_up_and_repo_status_installed(engines.dut, app_name, version)
