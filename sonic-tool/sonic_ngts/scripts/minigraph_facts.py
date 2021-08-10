0b895d2026dd21ba92cd1f30d7d885b1467b1e77
Sharon Lutati <slutati@nvidia.com>
2021-08-10 14:40:13 +0300

    Add "minigraph_neighbors" feild to canonical minigraph_facts mocker
    Details: this will field is on community minigraph facts and is requsted by some functions,
    Like neighbor_vm_restore, missing it causes Key Error.
    
    Change-Id: I27d9c39a5fde10cb7b6e0a06e4c0f3daf85a9826

--- sonic-tool/sonic_ngts/scripts/minigraph_facts.py
+++ sonic-tool/sonic_ngts/scripts/minigraph_facts.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from ansible.module_utils.basic import *
 import traceback
 import urllib
 import re
@@ -9,37 +10,38 @@ import json
 module: minigraph_facts
 author: Petro Pikh (petrop@nvidia.com)
 short_description: Module(emulates) which replaces original file ansible/library/minigraph_facts.py
-description: Instead of retrieve minigraph facts for a device from minigraph.xml - this module creating stub with 
+description: Instead of retrieve minigraph facts for a device from minigraph.xml - this module creating stub with
     data which are required by tests.
     As module argument we got hostname - using this hostname we find setup_name then we read config_db.json
     for specific setup and create dictionary with data(example below)
 
 Example of data which returned by this module:
 "ansible_facts": {
-    "minigraph_portchannels": {}, 
-    "minigraph_port_indices": {}, 
-    "minigraph_vlans": {}, 
-    "inventory_hostname": "r-leopard-32", 
-    "minigraph_ports": {"Ethernet8": {"alias": "etp3", "name": "Ethernet8"}, 
-    "Ethernet0": {"alias": "etp1", "name": "Ethernet0"}, "Ethernet4": {"alias": "etp2", "name": "Ethernet4"}, 
-    "Ethernet108": {"alias": "etp28", "name": "Ethernet108"}, "Ethernet100": {"alias": "etp26", "name": "Ethernet100"}, 
-    "Ethernet104": {"alias": "etp27", "name": "Ethernet104"}, "Ethernet68": {"alias": "etp18", "name": "Ethernet68"}, 
-    "Ethernet96": {"alias": "etp25", "name": "Ethernet96"}, "Ethernet124": {"alias": "etp32", "name": "Ethernet124"}, 
-    "Ethernet92": {"alias": "etp24", "name": "Ethernet92"}, "Ethernet120": {"alias": "etp31", "name": "Ethernet120"}, 
-    "Ethernet52": {"alias": "etp14", "name": "Ethernet52"}, "Ethernet56": {"alias": "etp15", "name": "Ethernet56"}, 
-    "Ethernet76": {"alias": "etp20", "name": "Ethernet76"}, "Ethernet72": {"alias": "etp19", "name": "Ethernet72"}, 
-    "Ethernet64": {"alias": "etp17", "name": "Ethernet64"}, "Ethernet32": {"alias": "etp9", "name": "Ethernet32"}, 
-    "Ethernet16": {"alias": "etp5", "name": "Ethernet16"}, "Ethernet36": {"alias": "etp10", "name": "Ethernet36"}, 
-    "Ethernet12": {"alias": "etp4", "name": "Ethernet12"}, "Ethernet88": {"alias": "etp23", "name": "Ethernet88"}, 
-    "Ethernet116": {"alias": "etp30", "name": "Ethernet116"}, "Ethernet80": {"alias": "etp21", "name": "Ethernet80"}, 
-    "Ethernet112": {"alias": "etp29", "name": "Ethernet112"}, "Ethernet84": {"alias": "etp22", "name": "Ethernet84"}, 
-    "Ethernet48": {"alias": "etp13", "name": "Ethernet48"}, "Ethernet44": {"alias": "etp12", "name": "Ethernet44"}, 
-    "Ethernet40": {"alias": "etp11", "name": "Ethernet40"}, "Ethernet28": {"alias": "etp8", "name": "Ethernet28"}, 
-    "Ethernet60": {"alias": "etp16", "name": "Ethernet60"}, "Ethernet20": {"alias": "etp6", "name": "Ethernet20"}, 
-    "Ethernet24": {"alias": "etp7", "name": "Ethernet24"}}, 
-    "minigraph_devices": {}, 
-    "minigraph_vlan_interfaces": [], 
-    "minigraph_hwsku": "ACS-MSN4700"
+    "minigraph_portchannels": {},
+    "minigraph_port_indices": {},
+    "minigraph_vlans": {},
+    "inventory_hostname": "r-leopard-32",
+    "minigraph_ports": {"Ethernet8": {"alias": "etp3", "name": "Ethernet8"},
+    "Ethernet0": {"alias": "etp1", "name": "Ethernet0"}, "Ethernet4": {"alias": "etp2", "name": "Ethernet4"},
+    "Ethernet108": {"alias": "etp28", "name": "Ethernet108"}, "Ethernet100": {"alias": "etp26", "name": "Ethernet100"},
+    "Ethernet104": {"alias": "etp27", "name": "Ethernet104"}, "Ethernet68": {"alias": "etp18", "name": "Ethernet68"},
+    "Ethernet96": {"alias": "etp25", "name": "Ethernet96"}, "Ethernet124": {"alias": "etp32", "name": "Ethernet124"},
+    "Ethernet92": {"alias": "etp24", "name": "Ethernet92"}, "Ethernet120": {"alias": "etp31", "name": "Ethernet120"},
+    "Ethernet52": {"alias": "etp14", "name": "Ethernet52"}, "Ethernet56": {"alias": "etp15", "name": "Ethernet56"},
+    "Ethernet76": {"alias": "etp20", "name": "Ethernet76"}, "Ethernet72": {"alias": "etp19", "name": "Ethernet72"},
+    "Ethernet64": {"alias": "etp17", "name": "Ethernet64"}, "Ethernet32": {"alias": "etp9", "name": "Ethernet32"},
+    "Ethernet16": {"alias": "etp5", "name": "Ethernet16"}, "Ethernet36": {"alias": "etp10", "name": "Ethernet36"},
+    "Ethernet12": {"alias": "etp4", "name": "Ethernet12"}, "Ethernet88": {"alias": "etp23", "name": "Ethernet88"},
+    "Ethernet116": {"alias": "etp30", "name": "Ethernet116"}, "Ethernet80": {"alias": "etp21", "name": "Ethernet80"},
+    "Ethernet112": {"alias": "etp29", "name": "Ethernet112"}, "Ethernet84": {"alias": "etp22", "name": "Ethernet84"},
+    "Ethernet48": {"alias": "etp13", "name": "Ethernet48"}, "Ethernet44": {"alias": "etp12", "name": "Ethernet44"},
+    "Ethernet40": {"alias": "etp11", "name": "Ethernet40"}, "Ethernet28": {"alias": "etp8", "name": "Ethernet28"},
+    "Ethernet60": {"alias": "etp16", "name": "Ethernet60"}, "Ethernet20": {"alias": "etp6", "name": "Ethernet20"},
+    "Ethernet24": {"alias": "etp7", "name": "Ethernet24"}},
+    "minigraph_devices": {},
+    "minigraph_vlan_interfaces": [],
+    "minigraph_hwsku": "ACS-MSN4700",
+    "minigraph_neighbors": {}
 }
 
 '''
@@ -77,9 +79,9 @@ def get_config_db_json_from_hostname(hostname):
     for html_line in setups_list_http_page_html_response:
         # Add CI setups to list, if will not find regular setup - then will iterate over CI setups list
         if 'CI_sonic_SPC' in html_line:
-            ci_setups.append(re.search('href="(\S*)\/"', html_line).groups()[0])
+            ci_setups.append(re.search(r'href="(\S*)\/"', html_line).groups()[0])
         if hostname in html_line:
-            setup_name = re.search('href="(\S*)\/"', html_line).groups()[0]
+            setup_name = re.search(r'href="(\S*)\/"', html_line).groups()[0]
             config_db_json = read_config_db_json(http_topo_base_path, setup_name)
             break
 
@@ -116,6 +118,7 @@ def generate_minigraph_facts(hostname):
     minigraph_facts["minigraph_vlans"] = {}
     minigraph_facts["minigraph_portchannels"] = {}
     minigraph_facts["minigraph_devices"] = {}
+    minigraph_facts["minigraph_neighbors"] = {}
 
     return minigraph_facts
 
@@ -139,8 +142,6 @@ def main():
         tb = traceback.format_exc()
         module.fail_json(msg=e.message + "\n" + tb)
 
-
-from ansible.module_utils.basic import *
 
 if __name__ == "__main__":
     main()
