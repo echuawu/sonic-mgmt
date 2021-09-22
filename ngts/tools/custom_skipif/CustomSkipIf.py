adfed3a8a73a1fc99b95b86055654d0778a9804b
Petro Pikh <petrop@nvidia.com>
2021-09-22 18:54:35 +0300

    Added possibility to ignore tests by branch name
    Implemented new functionality which allow to ignore tests by branch name: for example: 201911, 202012 or master
    
    Change-Id: I20b8ecdce177bd68a474c802a71d48e48e053fbd

--- ngts/tools/custom_skipif/CustomSkipIf.py
+++ ngts/tools/custom_skipif/CustomSkipIf.py
@@ -7,16 +7,18 @@ from infra.tools.topology_tools.topology_setup_utils import get_topology_by_setu
 
 CUSTOM_SKIP_IF_DICT = 'custom_skip_if_dict'
 CUSTOM_TEST_SKIP_PLATFORM_TYPE = 'dynamic_tests_skip_platform_type'
+CUSTOM_TEST_SKIP_BRANCH_NAME = 'dynamic_tests_skip_branch_name'
 
 logger = logging.getLogger()
 
 path = os.path.abspath(__file__)
 sonic_mgmt_path = path.split('/ngts/')[0]
-community_plugins_path = '/tests/common/plugins/'
+community_plugins_path = '/tests/common/plugins/custom_skipif'
 full_path_to_community_plugins = sonic_mgmt_path + community_plugins_path
 sys.path.append(full_path_to_community_plugins)
 
-from custom_skipif.CustomSkipIf import pytest_runtest_setup  # noqa: E402
+from CustomSkipIf import pytest_runtest_setup  # noqa: E402
+from Branch import get_branch_from_version  # noqa: E402
 
 
 def pytest_collection(session):
@@ -24,6 +26,17 @@ def pytest_collection(session):
     topology = get_topology_by_setup_name(session.config.option.setup_name, slow_cli=False)
     devdescription = topology.players['dut']['attributes'].noga_query_data['attributes']['Specific']['devdescription']
     platform = json.loads(devdescription).get('platform')
+    branch = get_branch(topology)
 
     session.config.cache.set(CUSTOM_SKIP_IF_DICT, None)
     session.config.cache.set(CUSTOM_TEST_SKIP_PLATFORM_TYPE, platform)
+    session.config.cache.set(CUSTOM_TEST_SKIP_BRANCH_NAME, branch)
+
+
+def get_branch(topology):
+    try:
+        show_version_raw_output = topology.players['dut']['engine'].run_cmd('show version')
+        branch = get_branch_from_version(show_version_raw_output)
+        return branch
+    except Exception as err:
+        logger.error('Unable to get branch name. Custom skip by branch impossible. Error: {}'.format(err))
