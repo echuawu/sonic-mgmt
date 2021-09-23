a0d69ac829447f103523c29a55cb642f3572d254
Petro Pikh <petrop@nvidia.com>
2021-09-23 16:19:59 +0300

    Changed execution order for APP Extension test "test_app_status"
    Now tes will be executed after "test_cpu_ram_hdd_usage.py"
    This change required - because sometimes time which required for app_ext docker to start can be more than 5 min,
    now test will run later - and app_ext docker will be already running
    
    Change-Id: I7711ed7fc76e4886c891290d48ce64f7722c0184

--- ngts/tests/push_build_tests/app_extension/test_app_ext_status.py
+++ ngts/tests/push_build_tests/app_extension/test_app_ext_status.py
@@ -4,6 +4,7 @@ import allure
 from ngts.tests.nightly.app_extension.app_extension_helper import verify_app_container_up_and_repo_status_installed
 
 
+@pytest.mark.order(after='tests/push_build_tests/system/test_cpu_ram_hdd_usage.py')
 @pytest.mark.app_ext
 @pytest.mark.build
 @pytest.mark.push_gate
