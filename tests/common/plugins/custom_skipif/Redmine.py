55caad127df0a8217d0dc24bd796511913dde18f
Petro Pikh <petrop@nvidia.com>
2021-10-25 11:15:04 +0300

    Updated GitHub and Redmine access tokens
    Change-Id: I68dd60821abc266bc29504d2bc855901ef31db60

--- tests/common/plugins/custom_skipif/Redmine.py
+++ tests/common/plugins/custom_skipif/Redmine.py
@@ -44,7 +44,7 @@ def get_issues_status(issues_list):
     """
     # TODO: use credentials from dedicated user
     redmine_api_url = 'https://redmine.mellanox.com/issues/mars_statuses.json'
-    api_key = '826fa9567c9f10072c450d0eda6c8a0517f1a228'
+    api_key = 'd6aa7057988319833df6a491c6f41cd8f7758e14'
     headers = {
         'Content-Type': "application/json",
         'X-Redmine-API-Key': api_key
