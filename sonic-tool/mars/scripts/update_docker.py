7fd3c5f74aa4997cd8bfe0d0e0cd6a187caea5db
Sharon Lutati <slutati@nvidia.com>
2021-11-14 11:38:59 +0200

    update devts version to 1.2.62
    Change-Id: I54dfd9cef067dc0b59b13022ed3d9ba3e51f66d8

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -374,7 +374,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.60'}
+    default_list = {'docker-ngts': '1.2.62'}
     return default_list.get(docker_name, latest)
 
 
