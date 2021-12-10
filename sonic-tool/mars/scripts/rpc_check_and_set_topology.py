5ace9907d1c45cfdfee196293ed704f4c1961ec8
Jibin Bao <jbao@nvidia.com>
2021-12-10 17:48:19 +0800

    Improve red flag issue for rpc topo check
    When topo is not expected topo, pre check will show red flag, but it is correct behaviour.
    Avoid confusion, so do this improvement
    
    Change-Id: If527fdc67a82d51ff8446dba469e533a575f5204

---
+++ sonic-tool/mars/scripts/rpc_check_and_set_topology.py
@@ -0,0 +1,69 @@
+# Builtin libs
+import argparse
+import os
+import sys
+
+# Third-party libs
+from fabric import Config
+from fabric import Connection
+
+# Home-brew libs
+from lib import constants
+from lib.utils import parse_topology, get_logger
+
+logger = get_logger("PreTestCheck")
+
+
+def _parse_args():
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--topo", dest="topo", help="Path to the MARS topology configuration file")
+    parser.add_argument("--dut-name", required=True, dest="dut_name", help="The DUT name")
+    parser.add_argument("--current_topo", required=True, dest="current_topo",
+                        help="Current topology for example: t0, t1, t1-lag, ptf32, ...")
+    parser.add_argument("--expected_topo", required=True, dest="expected_topo",
+                        help="Expected topology, for example: t0, t1, t1-lag, ptf32, ...")
+    parser.add_argument("--workspace-path", dest="workspace_path",
+                        help="Specify the location of sonic-mgmt repo")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = _parse_args()
+
+    workspace_path = args.workspace_path
+    repo_path = os.path.join(args.workspace_path, 'sonic-mgmt')
+    ansible_path = os.path.join(repo_path, "ansible")
+
+    current_topo = args.current_topo
+    expected_topo = args.expected_topo
+    dut_name = args.dut_name
+
+    if current_topo == expected_topo:
+        sys.exit()
+
+    topo = parse_topology(args.topo)
+    sonic_mgmt_device = topo.get_device_by_topology_id(constants.SONIC_MGMT_DEVICE_ID)
+    sonic_mgmt = Connection(sonic_mgmt_device.BASE_IP, user=sonic_mgmt_device.USERS[0].USERNAME,
+                            config=Config(overrides={"run": {"echo": True}}),
+                            connect_kwargs={"password": sonic_mgmt_device.USERS[0].PASSWORD})
+
+    try:
+        with sonic_mgmt.cd(ansible_path):
+            logger.info("Remove topo {}".format(current_topo))
+            cmd = "./testbed-cli.sh -k ceos remove-topo {SWITCH}-{TOPO} vault".format(SWITCH=dut_name, TOPO=current_topo)
+            logger.info("Running CMD: {}".format(cmd))
+            sonic_mgmt.run(cmd)
+    except Exception as err:
+        logger.info("Remove topo {} fail. Err log:{}".format(current_topo, err))
+
+    with sonic_mgmt.cd(ansible_path):
+        logger.info("Add topology")
+        cmd = "./testbed-cli.sh -k ceos add-topo {SWITCH}-{TOPO} vault".format(SWITCH=dut_name, TOPO=expected_topo)
+        logger.info("Running CMD: {}".format(cmd))
+        sonic_mgmt.run(cmd)
+
+    with sonic_mgmt.cd(ansible_path):
+        cmd = " ./testbed-cli.sh deploy-mg {SWITCH}-{TOPO} lab vault ".format(SWITCH=dut_name, TOPO=expected_topo)
+        logger.info("Running CMD: {}".format(cmd))
+        sonic_mgmt.run(cmd)
