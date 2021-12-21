38a7869deff2e987314f9f531fd40e51a125cebc
Jibin Bao <jbao@nvidia.com>
2021-12-21 19:11:34 +0800

    Update docker veresion
    Update docker version to 1.2.70, Becasue there is a improvment for validate_cmd
    http://10.7.77.140:8080/#/c/devts/+/104063/
    
    Change-Id: Ib5b98dc8bc559bbf4220ac883088f42b42e64193

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -374,7 +374,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.69'}
+    default_list = {'docker-ngts': '1.2.70'}
     return default_list.get(docker_name, latest)
 
 
