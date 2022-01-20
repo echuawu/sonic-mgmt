16e7bf935d810a61435134252928c0d57afe57c4
Anton <antonh@nvidia.com>
2022-01-20 17:56:57 +0200

    Remove temp workaround of not installed python packages
    The issue is fixed - https://redmine.mellanox.com/issues/2809993
    The workaround can be removed, change IDs of workaround:
     1. I6254b34ad2fab18b4c95ca26c0f17fbc5baab163
     2. I5e18c2d95d4fbe77186776863efcfd210f7722e9
    
    
    Change-Id: I6b7140f65a66d0aa778137729d1e586e2e1791b4
    Signed-off-by: Anton <antonh@nvidia.com>

--- sonic-tool/mars/scripts/deploy_and_upgrade.py
+++ sonic-tool/mars/scripts/deploy_and_upgrade.py
@@ -456,24 +456,6 @@ def deploy_minigprah(ansible_path, mgmt_docker_engine, dut_name, sonic_topo, rec
 
 
 @separate_logger
-def install_required_packages(ansible_path, sonic_topo, mgmt_docker_engine, setup_name):
-    """
-    This method will check and install required packages on DUT
-    """
-    logger.info("Check and install required packages on DUT")
-    cmd = "PYTHONPATH=/devts:{sonic_mgmt_dir} /ngts_venv/bin/pytest --setup_name={setup_name} --rootdir={sonic_mgmt_dir}/ngts" \
-          " -c {sonic_mgmt_dir}/ngts/pytest.ini --log-level=INFO --disable_loganalyzer --clean-alluredir --alluredir=/tmp/allure-results" \
-          " {sonic_mgmt_dir}ngts/scripts/extend_python_packges/extend_python_packages.py".\
-        format(sonic_mgmt_dir=constants.SONIC_MGMT_DIR, setup_name=setup_name)
-
-    with mgmt_docker_engine.cd(ansible_path):
-        logger.info("Running CMD: {}".format(cmd))
-        extend_python_packages_result = mgmt_docker_engine.run(cmd, warn=True)
-        if extend_python_packages_result.failed:
-            logger.error('Installation of required packages failed.')
-
-
-@separate_logger
 def post_install_check(ansible_path, mgmt_docker_engine, dut_name, sonic_topo):
     """
     Method which doing post install checks: check ports status, check dockers status, etc.
@@ -622,10 +604,6 @@ def main():
     install_image(ansible_path=ansible_path, mgmt_docker_engine=mgmt_docker_engine,
                   sonic_topo=args.sonic_topo, setup_name=args.setup_name,
                   image_url=base_version_url, upgrade_type=args.upgrade_type, dut_name=args.dut_name)
-
-    time.sleep(30)
-    install_required_packages(ansible_path=ansible_path, mgmt_docker_engine=mgmt_docker_engine,
-                              sonic_topo=args.sonic_topo, setup_name=args.setup_name)
 
     # Community only steps
     if args.sonic_topo != 'ptf-any':
