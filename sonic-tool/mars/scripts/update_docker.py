e8e4aa78e843db908c743036e6a2948e7b80aebc
Yana Port <yport@nvidia.com>
2022-02-15 14:56:46 +0200

    NGTS docker is updated to 1.7.73
    Change-Id: Icd5040e28461f31c4af005e296c0989df10c6ec0

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -381,7 +381,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.72'}
+    default_list = {'docker-ngts': '1.2.73'}
     return default_list.get(docker_name, latest)
 
 def send_takeover_notification(topo):
