65e5d4a8a2397b9ca492b07d979ea09cd17b792e
Petro Pikh <petrop@nvidia.com>
2022-03-03 11:46:30 +0200

    Increased timeout for pushgate test in case of reboot to 5400 sec
    PushGate reboot test took more than 1 hr, increased timeout to 1 hr 30 min
    
    Change-Id: Iac2fba70c6ba22752cdc1a02dc35e41fa3131dc5

--- ngts/tests/push_build_tests/system/test_reboot_reload.py
+++ ngts/tests/push_build_tests/system/test_reboot_reload.py
@@ -235,7 +235,7 @@ class RebootReload:
     def do_func_validations(request):
         pytest_args_list = list(request.config.invocation_params.args)
         pytest_run_cmd = prepare_pytest_args(pytest_args_list)
-        out, err, rc = run_process_on_host(pytest_run_cmd, timeout=3600)
+        out, err, rc = run_process_on_host(pytest_run_cmd, timeout=5400)
         generate_report(out, err)
         if rc:
             raise AssertionError('Functional validation failed, please check logs')
