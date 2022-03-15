102af9cf718d2b7139ad2a18d46fd1b36362fa1a
Sharon Lutati <slutati@nvidia.com>
2022-03-15 09:55:21 +0200

    Update ngts devts docker version to 1.2.75
    Change-Id: Ic161056e76cc157c8e6872fe7e451719738677c9

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -381,7 +381,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.73'}
+    default_list = {'docker-ngts': '1.2.75'}
     return default_list.get(docker_name, latest)
 
 def send_takeover_notification(topo):
