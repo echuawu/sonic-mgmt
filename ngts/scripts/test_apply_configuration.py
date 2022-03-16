6f19217ca7114c80ae8be66288fbb12f08f5aa09
Sharon Lutati <slutati@nvidia.com>
2022-03-16 10:08:40 +0200

    Update script to check dockers are up after configuring switch
    Details:
    The following script is used in the bringup_containers script to configure the simx switch,
    before setting the connectivities of the setup.
    In case dockers on the switch are failing after configuration,
    the split tap devices won't be created and the bring up containers will fail on connection configuration,
    when in fact the real issue is that the dockers on the switch are down.
    this way the script will give true indication for what is the issue.
    
    Change-Id: I0e681674a4f6ba01d79d9709fc993ff161e6a006

--- ngts/scripts/test_apply_configuration.py
+++ ngts/scripts/test_apply_configuration.py
@@ -2,7 +2,7 @@
 import allure
 import logging
 import pytest
-from ngts.cli_wrappers.sonic.sonic_general_clis import SonicGeneralCli
+from ngts.constants.constants import SonicConst
 
 
 logger = logging.getLogger()
@@ -23,6 +23,7 @@ def test_apply_basic_conf(topology_obj, setup_name, platform_params):
         dut_engine = topology_obj.players['dut']['engine']
         cli_object = topology_obj.players['dut']['cli']
         with allure.step("Apply port_config.ini and config_db.json"):
-                SonicGeneralCli().apply_basic_config(topology_obj, dut_engine, cli_object, setup_name, platform_params)
+            cli_object.general.apply_basic_config(topology_obj, dut_engine, cli_object, setup_name, platform_params)
+            cli_object.general.verify_dockers_are_up(dut_engine, SonicConst.DOCKERS_LIST)
     except Exception as err:
         raise AssertionError(err)
