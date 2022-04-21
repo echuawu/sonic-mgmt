ca4cca5e532726170c59567f023771f762665ffc
Cong Hou <congh@nvidia.com>
2022-04-21 11:56:42 +0800

    Revert the change of fib test for supporting multi-asic
    There are 2 commit to fix the issue about multi-asic fib test which was introduced by community PR #5027:
    http://10.7.77.140:8080/c/switchx/sonic/sonic-mgmt/+/109148
    http://10.7.77.140:8080/c/switchx/sonic/sonic-mgmt/+/109157
    But #5027 was reverted by community, so we need to revert the 2 local commits too.
    Because the commit 109148 contains another irrelevant change, so commit a new change instead of git revert.
    
    Change-Id: I850fa9337964fe81eed51a5ed27e5d775b9c0a89

--- tests/common/fixtures/fib_utils.py
+++ tests/common/fixtures/fib_utils.py
@@ -188,15 +188,15 @@ def get_fib_info(duthost, dut_cfg_facts, duts_mg_facts):
                             if 'role' in ports[po[ifname]['members'][0]] and ports[po[ifname]['members'][0]]['role'] == 'Int':
                                 skip = True
                             else:
-                                oports.append([str(duts_mg_facts[asic_index]['minigraph_ptf_indices'][x]) for x in po[ifname]['members']])
+                                oports.append([str(duts_mg_facts['minigraph_ptf_indices'][x]) for x in po[ifname]['members']])
                     else:
                         if sub_interfaces.has_key(ifname):
-                            oports.append([str(duts_mg_facts[asic_index]['minigraph_ptf_indices'][ifname.split('.')[0]])])
+                            oports.append([str(duts_mg_facts['minigraph_ptf_indices'][ifname.split('.')[0]])])
                         elif ports.has_key(ifname):
                             if 'role' in ports[ifname] and ports[ifname]['role'] == 'Int':
                                 skip = True
                             else:
-                                oports.append([str(duts_mg_facts[asic_index]['minigraph_ptf_indices'][ifname])])
+                                oports.append([str(duts_mg_facts['minigraph_ptf_indices'][ifname])])
                         else:
                             logger.info("Route point to non front panel port {}:{}".format(k, v))
                             skip = True
