9cdd0e6f93ea1808e3818cf31c088f1504207d2f
Yana Port <yport@nvidia.com>
2022-05-02 14:31:44 +0300

    NVOS - .cases updated
    Change-Id: I11e1e80f0788fc9bd5b93915e65043146c70e2c2

--- ngts/tests_nvos/general/test_adjust_restore_switch_for_community_tests.py
+++ ngts/tests_nvos/general/test_adjust_restore_switch_for_community_tests.py
@@ -9,24 +9,24 @@ def test_adjust_switch_for_community_tests(topology_obj):
     """
     Removes 2 first lines from 'show command' output.
 
-    flow:
-    1. Rename existing show script to show.orig
-    2. Create and save new show script
+     flow:
+     1. Rename existing show script to show.orig
+     2. Create and save new show script
     :param topology_obj: topology_obj fixture
     """
     engine = topology_obj.players['dut']['engine']
     with allure.step("Rename original show script to show.orig"):
-        new_show_path = engine.run_cmd("which show")
-        origin_show_path = new_show_path + '.orig'
+        path_to_show_cmd = engine.run_cmd("which show")
+        path_to_show_cmd_orig = path_to_show_cmd + '.orig'
         # sudo cp /usr/local/bin/show /usr/local/bin/show.orig
-        engine.run_cmd("sudo cp {origin} {new_name}".format(origin=new_show_path, new_name=origin_show_path))
+        engine.run_cmd("sudo cp {origin} {new_name}".format(origin=path_to_show_cmd, new_name=path_to_show_cmd_orig))
 
     with allure.step("Create new show script"):
-        engine.run_cmd("sudo rm {}".format(new_show_path))
-        line_to_write = '{origin_path} $1 $2 $3 | tail -n +3'.format(origin_path=origin_show_path)
-        engine.run_cmd("sudo touch {origin_path}".format(origin_path=new_show_path))
-        engine.run_cmd("sudo chmod 777 {origin_path}".format(origin_path=new_show_path))
-        engine.run_cmd("sudo echo \'{line}\' >> {file}".format(line=line_to_write, file=new_show_path))
+        engine.run_cmd("sudo rm {}".format(path_to_show_cmd))
+        line_to_write = '{origin_path} $1 $2 $3 | tail -n +3'.format(origin_path=path_to_show_cmd_orig)
+        engine.run_cmd("sudo touch {origin_path}".format(origin_path=path_to_show_cmd))
+        engine.run_cmd("sudo chmod 777 {origin_path}".format(origin_path=path_to_show_cmd))
+        engine.run_cmd("sudo echo \'{line}\' >> {file}".format(line=line_to_write, file=path_to_show_cmd))
 
 
 def test_restore_origin_setup_after_adjustments_for_community_tests(topology_obj):
