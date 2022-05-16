ed92aacc70b1b8d6d73973c953a8a64648fdd521
Ihor Chekh <ichekh@nvidia.com>
2022-05-16 17:29:38 +0300

    Added new community setup r-tigon-21.
    Change-Id: Ie507a35190e68e91ecb587e1844266e37854ed02

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -381,7 +381,7 @@ def get_port_alias_to_name_map(hostname, hwsku, asic_id=None):
             port_alias_to_name_map["etp63b"] = "Ethernet250"
             port_alias_to_name_map["etp64a"] = "Ethernet252"
             port_alias_to_name_map["etp64b"] = "Ethernet254"
-        elif hostname in ["r-tigon-11", "r-tigon-20"]:
+        elif hostname in ["r-tigon-11", "r-tigon-20", "r-tigon-21"]:
             for i in range(1, 65):
                 port_alias_to_name_map["etp%d" % i] = "Ethernet%d" % ((i - 1) * 4)
         elif hostname in ["r-leopard-01" , "r-leopard-58"]:
