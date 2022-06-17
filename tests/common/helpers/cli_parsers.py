3ac4dffb0b4806f40449a23162c8269ae821a2dd
Nana He <nhe@nvidia.com>
2022-06-17 23:17:57 +0800

    Add rif loopback action tests 4 test cases added: 1. test_loopback_action_basic 2. test_loopback_action_port_flap 3. test_loopback_action_reload 4. def test_loopback_action_wjh, this will not be upstreamed.
    Change-Id: Id3e769e2f6852bdc9aa30250cc3f4eca4f83ad2d

---
+++ tests/common/helpers/cli_parsers.py
@@ -0,0 +1,158 @@
+import logging
+
+logger = logging.getLogger()
+
+
+def generic_sonic_output_parser(output, headers_ofset=0, len_ofset=1, data_ofset_from_start=2,
+                                data_ofset_from_start_end=None, data_ofset_from_end=None,
+                                column_ofset=2, output_key=None, header_line_number=1):
+    """
+    This method doing parse for command output and provide dictionary or list of dictionaries with parsed results.
+    Method works only in case when cmd output has structure like below
+    :param output: command output which should be parsed, example:
+    Capability codes: (R) Router, (B) Bridge, (O) Other                                                 <<< This line we can skip by ofset args
+    LocalPort    RemoteDevice             RemotePortID       Capability    RemotePortDescr              <<< Mandatory line
+    -----------  -----------------------  -----------------  ------------  -------------------------    <<< Mandatory line
+    Ethernet0    r-sonic-10-006           0c:42:a1:88:0a:1c  R             Interface   8 as enp5s0f0
+    :param headers_ofset: Line number in which we have headers, in example above it is line 1(in real it is 2, but in python it is 1)
+    :param len_ofset: Line number from which we can find len for all fields, in example above it is line 2
+    :param data_ofset_from_start: Line number from which we will start parsing data and fill dictionary with results
+    :param data_ofset_from_start_end: Line number from start till which we will do parse(parameter is optional)
+    :param data_ofset_from_end: Line number from end till which we will do parse(parameter is optional)
+    :param column_ofset: Number of spaces between columns in output(usually 2)
+    :param output_key: parameter which specify which key should be used in output(from example above can be used: LocalPort,
+    RemoteDevice, RemotePortID, Capability, RemotePortDescr). If NONE - than we we will return list
+    :param header_line_number: the number of the header lines, usually the number is 1, but for some command, it will be more than 1
+    :return: dictionary, example for output of "show lldp table" with args for method: headers_ofset=1,
+                                                                                       len_ofset=2,
+                                                                                       data_ofset_from_start=3,
+                                                                                       data_ofset_from_end=-2,
+                                                                                       output_key='LocalPort'
+    {'Ethernet0': {'LocalPort': 'Ethernet0', 'RemoteDevice': 'r-sonic-10-006', ' RemotePortID': '0c:42:a1:88:0a:1c',
+    ' Capability': 'R', 'RemotePortDescr': 'Interface   8 as enp5s0f0'}, 'Ethernet8': {'LocalPort': 'Ethernet8',
+    'RemoteDevice': 'r-ocelot-02',...................}}
+    Or LIST can be returned, example: [{'LocalPort': 'Ethernet0', 'RemoteDevice': 'r-sonic-10-006',
+    ' RemotePortID': '0c:42:a1:88:0a:1c', ' Capability': 'R', 'RemotePortDescr': 'Interface   8 as enp5s0f0'},
+    {'LocalPort': 'Ethernet8', 'RemoteDevice': 'r-ocelot-02',........}]
+    """
+    # Get all headers
+    headers_lines = output.splitlines()[
+        headers_ofset: headers_ofset +
+        header_line_number]
+
+    """Get lens for each column according to "---------" symbols len in output
+    Interface    Oper    Admin    Alias    Description
+    -----------  ------  -------  -------  -------------    <<<<<< This will be used
+    Ethernet0      up       up     etp1            N/A
+    """
+    column_lens = output.splitlines()[len_ofset].split()
+
+    # Parse only lines from "data_ofset_from_start" and if
+    # "data_ofset_from_end" exist - then parse till the "data_ofset_from_end"
+    data = output.splitlines()[data_ofset_from_start:]
+    if data_ofset_from_end and not data_ofset_from_start_end:
+        data = output.splitlines()[data_ofset_from_start:data_ofset_from_end]
+    if data_ofset_from_start_end and not data_ofset_from_end:
+        data = output.splitlines()[data_ofset_from_start:data_ofset_from_start_end]
+
+    result_dict = {}
+    result_list = []
+    last_line_key = ""
+    for line in data:
+        if line == '':
+            continue
+        first_column_item = line[:len(column_lens[0])].strip()
+        if first_column_item:
+            line_dict = {}
+        parse_line_data_to_dict(line, line_dict, column_lens, headers_lines, column_ofset)
+        if output_key:
+            last_line_key = update_result_dict(line_dict, output_key, last_line_key, result_dict)
+        elif first_column_item:
+            result_list.append(line_dict)
+
+    if output_key:
+        return result_dict
+    else:
+        return result_list
+
+
+def parse_line_data_to_dict(line, line_dict, column_lens, headers_lines, column_ofset):
+    """
+    Parse the line data from string to the dict, the line data will be updated to line_dict.
+    :param line: the origin line data in string format
+    :param line_dict: dictionary which is used to save the data in the line in string format
+    :param column_lens: the column length list
+    :param headers_lines: the headers name content
+    :param column_ofset: Number of spaces between columns in output(usually 2)
+    :return: None
+        Example: the line can be one of line of the following content, for the RULE_1, it has more than one line,
+        in this function need to do the special process of it.
+            Table                  Rule    Priority    Action    Match
+            ---------------------  ------  ----------  --------  --------------------------
+            DATA_EGRESS_L3TEST     RULE_1  9999        FORWARD   ETHER_TYPE: 2048
+                                                                 SRC_IP: 30.0.0.2/32
+            DATA_EGRESS_L3V6TEST   RULE_1  9999        FORWARD   SRC_IPV6: 80c0:a800::2/128
+    """
+    base_position = 0
+    for column_len in column_lens:
+        new_position = base_position + len(column_len)
+        header_name = get_column_header_name(headers_lines, base_position, new_position)
+        item_value = line[base_position:new_position].strip()
+        if header_name in line_dict:
+            # if line_dict has header_name, it means that one item occupy more than one line,
+            # then the corresponding value should be a list
+            if item_value:
+                if not isinstance(line_dict[header_name], list):
+                    line_dict[header_name] = [line_dict[header_name]]
+                line_dict[header_name].append(item_value)
+        else:
+            line_dict[header_name] = item_value
+        base_position = new_position + column_ofset
+    return line_dict
+
+
+def get_column_header_name(headers_lines, base_position, new_position):
+    """
+    get the full column header name
+    :param headers_lines: how many lines of the header content
+    :param base_position: start position of the column header
+    :param new_position: end position of the column header
+    :return: column header name
+    """
+
+    header_name = ""
+    for headers_line in headers_lines:
+        tmp = headers_line[base_position:new_position].strip()
+        if tmp:
+            if header_name:
+                header_name = header_name + " " + \
+                    headers_line[base_position:new_position].strip()
+            else:
+                header_name = tmp
+    return header_name
+
+
+def update_result_dict(line_dict, key_name, last_line_key, result_dict):
+    """
+    update the result dict
+    :param line_dict: dict format value of one line in the return of the show command
+    :param key_name: the output key of the result dict, it is one of the column header name
+    :param last_line_key: the key value used in the previous line.
+    :param result_dict: the result dict which need to be updated
+    :return: the output key value
+    """
+    if line_dict[key_name]:
+        # if the corresponding column value is not empty, then it should be the
+        # first line of the value
+        line_key = line_dict[key_name]
+        result_dict[line_key] = line_dict
+    else:
+        # if the corresponding column value is empty, then it should be not
+        # first line of the value
+        last_line_dict = result_dict[last_line_key]
+        line_key = last_line_key
+
+        for key in last_line_dict.keys():
+            if line_dict[key]:
+                last_line_dict[key] = " ".join([last_line_dict[key], line_dict[key]])
+    return line_key
---
+++ tests/iface_loopback_action/test_iface_loopback_action_wjh.py
@@ -0,0 +1,125 @@
+import logging
+import pytest
+from .iface_loopback_action_helper import ACTION_DROP, NUM_OF_TOTAL_PACKETS
+from .iface_loopback_action_helper import verify_traffic
+from .iface_loopback_action_helper import config_loopback_action
+from .iface_loopback_action_helper import clear_rif_counter
+from .iface_loopback_action_helper import verify_interface_loopback_action
+from .iface_loopback_action_helper import verify_rif_tx_err_count
+from tests.common.helpers.assertions import pytest_assert
+from tests.common.helpers.cli_parsers import generic_sonic_output_parser
+from tests.common.plugins.allure_wrapper import allure_step_wrapper as allure
+
+
+pytestmark = [pytest.mark.topology('any')]
+logger = logging.getLogger(__name__)
+allure.logger = logger
+
+
+@pytest.fixture(scope='module', autouse=True)
+def check_global_configuration(duthost):
+    config_facts = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
+    wjh_global = config_facts.get('WJH', {})
+
+    try:
+        wjh_global = wjh_global['global']
+        if wjh_global['mode'] != 'debug':
+            pytest.skip("Debug mode is not enabled. Skipping test.")
+    except Exception as e:
+        pytest.fail("Could not fetch global configuration information.")
+
+
+@pytest.fixture(scope='module', autouse=True)
+def check_feature_enabled(duthost):
+    features = duthost.feature_facts()['ansible_facts']['feature_facts']
+    if 'what-just-happened' not in features or features['what-just-happened'] != 'enabled':
+        pytest.skip("what-just-happened feature is not available. Skipping the test.")
+
+
+def test_loopback_action_wjh(duthost, ptfadapter, ports_configuration):
+    rif_interfaces = list(ports_configuration.keys())
+    intf_count = len(rif_interfaces)
+    action_list = [ACTION_DROP] * intf_count
+    count_list = [NUM_OF_TOTAL_PACKETS] * intf_count
+    with allure.step("Configure the loopback action to drop"):
+        config_loopback_action(duthost, rif_interfaces, action_list)
+    with allure.step("Verify the loopback acton is correct of port flap"):
+        with allure.step("Check the looback action is configured correctly with cli command"):
+            verify_interface_loopback_action(duthost, rif_interfaces, action_list)
+        with allure.step("Check the loopback traffic"):
+            with allure.step("Clear the rif counter"):
+                clear_rif_counter(duthost)
+            with allure.step("Check the traffic can be received or dropped as expected"):
+                verify_traffic(duthost, ptfadapter, rif_interfaces, ports_configuration, action_list)
+            with allure.step("Check the TX_ERR in rif counter statistic will increase or not as expected"):
+                verify_rif_tx_err_count(duthost, rif_interfaces, count_list)
+            with allure.step("Check WJH L3 drop group"):
+                verify_wjh_l3_drop_group(duthost, rif_interfaces, ports_configuration)
+
+
+def get_wjh_loopback_drop_items(duthost):
+    """
+    Get the wjh L3 loopback action drop items, Packets that will dropped due to loopback action will be counted by
+    WJH in L3 drop group in severity warning.
+    :param duthost: DUT hst object
+    :return: Dictionary of L3 loopback action drop items
+    Examples:
+    {'Ethernet244': [{'#': '681',
+                      'Timestamp': '22/06/17 15:25:24.323',
+                      'sPort': 'Ethernet244',
+                      'dPort': 'N/A', 'VLAN': '58',
+                      'sMAC': 'ee:46:86:41:a4:3d',
+                      'dMAC': '1c:34:da:1d:ea:00',
+                      'EthType': 'Dot1Q',
+                      'Src IP:Port': '11.11.11.11',
+                      'Dst IP:Port': '11.8.0.10',
+                      'IP Proto': 'ip',
+                      'Drop Group': 'L3',
+                      'Severity': 'Warn',
+                      'Drop reason - Recommended action': 'Router interface loopback - Validate the interface configuration'
+                      },
+                     ...],
+      'Ethernet248': [{'#': '691',
+                       'Timestamp': '22/06/17 15:25:27.234',
+                       'sPort': 'Ethernet248',
+                       'dPort': 'N/A',
+                       'VLAN': '59',
+                       'sMAC': 'de:50:79:3b:04:3e',
+                       'dMAC': '1c:34:da:1d:ea:00',
+                       'EthType': 'Dot1Q',
+                       'Src IP:Port': '11.11.11.11',
+                       'Dst IP:Port': '11.9.0.10',
+                       'IP Proto': 'ip',
+                       'Drop Group': 'L3',
+                       'Severity': 'Warn',
+                       'Drop reason - Recommended action': 'Router interface loopback - Validate the interface configuration'
+                       },
+                    ...],
+                    ...
+    }
+    """
+    stdout = duthost.shell("show what-just-happened poll forwarding")
+    if stdout['rc'] != 0:
+        raise Exception(stdout['stdout'] + stdout['stderr'])
+    table = stdout['stdout']
+    wjh_drop_items = generic_sonic_output_parser(table, header_line_number=2, len_ofset=2, data_ofset_from_start=3, column_ofset=1)
+    wjh_loopback_drop_items = {}
+    for item in wjh_drop_items:
+        item['Drop reason - Recommended action'] = " ".join(item['Drop reason - Recommended action'])
+        port = item['sPort']
+        if item['Drop Group'] == 'L3' and item['Drop reason - Recommended action'] == \
+                'Router interface loopback - Validate the interface configuration':
+            if port not in wjh_loopback_drop_items.keys():
+                wjh_loopback_drop_items[port] = []
+            wjh_loopback_drop_items[port].append(item)
+    return wjh_loopback_drop_items
+
+
+def verify_wjh_l3_drop_group(duthost, expect_drop_rif_interface, ports_configuration):
+    wjh_loopback_drop_items = get_wjh_loopback_drop_items(duthost)
+    for rif_interface in expect_drop_rif_interface:
+        port = ports_configuration[rif_interface]['port']
+        pytest_assert(port in wjh_loopback_drop_items.keys(),
+                      "Packets that dropped due to loopback action is not counted by WJH in L3 drop group "
+                      "in severity warning for {}".format(rif_interface))
+
