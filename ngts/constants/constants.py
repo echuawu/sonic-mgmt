04a4bb74c32768dc17a11ee0a3c57c62bfe6b315
Nana He <nhe@nvidia.com>
2022-06-24 18:52:29 +0800

    Add support for remove t0-56 topo before setup deployment
    The the recover_topology function, need to remove all the possible toplogy, add t0-56 topo which is also a possible topo
    
    Change-Id: I106422441049fb27f01b830619daf80492373adf

--- ngts/constants/constants.py
+++ ngts/constants/constants.py
@@ -748,7 +748,7 @@ class MarsConstants:
     VER_SDK_PATH = "/opt/ver_sdk"
     EXTRA_PACKAGE_PATH_LIST = ["/usr/lib64/python2.7/site-packages"]
 
-    TOPO_ARRAY = ("t0", "t1", "t1-lag", "ptf32", "t0-64", "t1-64-lag")
+    TOPO_ARRAY = ("t0", "t1", "t1-lag", "ptf32", "t0-64", "t1-64-lag", "t0-56")
     REBOOT_TYPES = {
         "reboot": "reboot",
         "fast-reboot": "fast-reboot",
