3bcbab5f92644e82c0e0e49b856cb56626c201b5
Petro Pikh <petrop@nvidia.com>
2022-06-27 16:59:14 +0300

    Updated docker-ngts version to version 1.2.91
    Change-Id: I8b804c188a701851e834bfbabf1aff13bf0a164c

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -384,7 +384,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.89'}
+    default_list = {'docker-ngts': '1.2.91'}
     return default_list.get(docker_name, latest)
 
 def send_takeover_notification(topo):
