cbbb4867969f532c7ca3bfacf3ba87b328a783bb
Nana He <nhe@nvidia.com>
2022-08-10 10:09:52 +0800

    Add DEVICE_NEIGHBOR_METADATA table for 'Mellanox-SN4600C-C64' when topo is ptf32
    The purpose for adding this table is to support T1 for dual ToR scenario in ptf32 topo, so the additional lossless PG can be created.
    After the change, the additonal lossless PG related test case can start to run
    
    Change-Id: Ib29951f940040577ced74efd7afbb6b3fd4d654f

--- sonic-tool/mars/scripts/rpc_check_and_set_topology.py
+++ sonic-tool/mars/scripts/rpc_check_and_set_topology.py
@@ -36,6 +36,44 @@ def get_sonic_branch(duthost):
     return branch.strip()
 
 
+def get_sonic_hwsku(duthost):
+    return duthost.run('redis-cli -n 4 hget "DEVICE_METADATA|localhost" hwsku').stdout.strip()
+
+
+def update_device_neighbor_metadata(duthost, topo):
+    """
+    Hack the DEVICE_NEIGHBOR_METADATA table for the ptf32 topo on "Mellanox-SN4600C-C64" to simulate the
+    dual tor qos scenario
+    :param duthost: dut host engine
+    :param topo: topology value, expect ptf32 topo
+    """
+    # need to make sure that the ssh connection to the dut is still active, call the open directly.
+    sonic_dut.open()
+    hwsku = get_sonic_hwsku(sonic_dut)
+    if topo != "ptf32" or hwsku != "Mellanox-SN4600C-C64":
+        return
+    neighbors = duthost.run("redis-cli -n 4 keys DEVICE_NEIGHBOR*").stdout.strip("\n").split("\n")
+    vm_hwsku = "Arista-VM"
+    lo_addr = None
+    add_neighbor_metadata_cmd_pattern = 'redis-cli -n 4 hset "DEVICE_NEIGHBOR_METADATA|{}" hwsku {} lo_addr {} mgmt_addr {}  type {}'
+    mgmt_addr_patern = "10.75.207.{}"
+    for index, neighbor in enumerate(neighbors):
+        neighbor_name = sonic_dut.run('redis-cli -n 4 hget \"{}\" name'.format(neighbor)).stdout.strip("\n")
+        mgmt_addr = mgmt_addr_patern.format(10+index)
+        if neighbor_name.endswith("T0"):
+            router_type = "ToRRouter"
+        else:
+            router_type = "SpineRouter"
+        add_neighbor_metadata_cmd = add_neighbor_metadata_cmd_pattern.format(neighbor_name, vm_hwsku, lo_addr, mgmt_addr, router_type)
+        sonic_dut.run(add_neighbor_metadata_cmd)
+    enable_qos_remap_table = 'redis-cli -n 4 hset "SYSTEM_DEFAULTS|tunnel_qos_remap" status enabled'
+    sonic_dut.run(enable_qos_remap_table)
+    sonic_dut.run("sudo config qos reload --no-dynamic-buffer")
+    sonic_dut.run("sudo config save -y")
+    sonic_dut.run("sudo config reload -y")
+    sonic_dut.run("sleep 180")
+
+
 if __name__ == "__main__":
     args = _parse_args()
 
@@ -86,3 +124,10 @@ if __name__ == "__main__":
         cmd = "./testbed-cli.sh deploy-mg {SWITCH}-{TOPO} lab vault".format(SWITCH=dut_name, TOPO=expected_topo)
         logger.info("Running CMD: {}".format(cmd))
         sonic_mgmt.run(cmd)
+
+    with sonic_mgmt.cd(ansible_path):
+        cmd = "ansible-playbook -i inventory --limit {SWITCH} post_upgrade_check.yml -e topo={TOPO} -b -vvv ".format(SWITCH=dut_name, TOPO=expected_topo)
+        logger.info("Running CMD: {}".format(cmd))
+        sonic_mgmt.run(cmd)
+
+    update_device_neighbor_metadata(sonic_dut, expected_topo)
