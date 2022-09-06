3d836c0a2346a6b3e9ddb672b94946f44924b142
Cong Hou <congh@nvidia.com>
2022-09-06 18:45:06 +0800

    Add compatibility for dualtor setup when getting topology from noga
    The lower tor dut in a dualtor setup uses 'dut-b' as the description in noga, and which is used by the topology object as the key of the dut object. Need to update the logic for getting topology from noga to be compatible with dualtor setups, otherwise all codes use topology.players['dut'] will get key error.
    
    Change-Id: Iec6441fd575afa2cf97dcf5589f54689d4505793

--- ngts/tools/topology_tools/topology_by_setup.py
+++ ngts/tools/topology_tools/topology_by_setup.py
@@ -10,6 +10,13 @@ def get_topology_by_setup_name_and_aliases(setup_name, slow_cli):
 
 def update_dut_alias(topology):
     if 'dut' not in topology.players.keys():
+        # For the lower tor in dual-tor topology, the key of dut name got from noga is 'dut-b'
+        # If the topology only has key 'dut-b' without key 'dut', means it is deployed as a normal setup
+        # Change the 'dut-b' to 'dut' in this case
+        if 'dut-b' in topology.players.keys():
+            topology.players['dut'] = topology.players.pop('dut-b')
+            topology.players['dut_serial'] = topology.players.pop('dut-b_serial')
+            topology.players['dut']['attributes'].noga_query_data['attributes']['Common']['Description'] = 'dut'
         for alias in PlayeresAliases.Aliases_list:
             if alias in topology.players.keys():
                 topology.players['dut'] = topology.players[alias]
