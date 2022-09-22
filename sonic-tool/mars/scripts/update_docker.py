c4d1b597cc721bb371e585bd173184299137118a
Petro Pikh <petrop@nvidia.com>
2022-09-22 16:45:24 +0300

    Updated NGTS docker version to 1.2.123
    Change-Id: I421c782654fc7957e2a3b262f8fb4c74b685f69f

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -386,7 +386,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.118'}
+    default_list = {'docker-ngts': '1.2.123'}
     return default_list.get(docker_name, latest)
 
 
