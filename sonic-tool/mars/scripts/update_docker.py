2b9266d577a3b3033c4b0f8894fb3a3683fc1282
Petro Pikh <petrop@nvidia.com>
2022-09-29 15:15:00 +0300

    Updated NGTS docker version to 1.2.125
    Version 1.2.125 based on new Ubuntu 20.04 community sonic-mgmt docker
    
    Change-Id: Ibd535b7c40d30ae07861b2d238f9ba8c4fa75869

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -386,7 +386,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.123'}
+    default_list = {'docker-ngts': '1.2.125'}
     return default_list.get(docker_name, latest)
 
 
