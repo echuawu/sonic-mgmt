bd3dcee66f5b54814fbe322325c478c3f18bfabb
Cong Hou <congh@nvidia.com>
2022-10-08 17:51:05 +0800

    Improve the ptf log in allre report
    1. Add log/pcap file names to the allure title
    2. Attach traceback message to allure report when ptf test fails
    
    Change-Id: I7e5445040b83cf26ef75b3b94a60c44b64260d5e

--- tests/ptf_runner.py
+++ tests/ptf_runner.py
@@ -8,7 +8,7 @@ from datetime import datetime
 logger = logging.getLogger(__name__)
 
 
-def ptf_collect(host, log_file, cmd_result='No result collected.'):
+def ptf_collect(host, log_file):
     pos = log_file.rfind('.')
     filename_prefix = log_file[0:pos] if pos > -1 else log_file
 
@@ -17,14 +17,13 @@ def ptf_collect(host, log_file, cmd_result='No result collected.'):
     suffix = str(datetime.utcnow()).replace(' ', '.')
     filename_log = './logs/ptf_collect/' + rename_prefix + '.' + suffix + '.log'
     host.fetch(src=log_file, dest=filename_log, flat=True, fail_on_missing=False)
-    allure.attach.file(filename_log, 'ptf_log', allure.attachment_type.TEXT)
+    allure.attach.file(filename_log, 'ptf_log: ' + filename_log, allure.attachment_type.TEXT)
     pcap_file = filename_prefix + '.pcap'
     output = host.shell("[ -f {} ] && echo exist || echo null".format(pcap_file))['stdout']
     if output == 'exist':
         filename_pcap = './logs/ptf_collect/' + rename_prefix + '.' + suffix + '.pcap'
         host.fetch(src=pcap_file, dest=filename_pcap, flat=True, fail_on_missing=False)
-        allure.attach.file(filename_pcap, 'ptf_pcap', allure.attachment_type.PCAP)
-    allure.attach(cmd_result, 'ptf_console_result', allure.attachment_type.TEXT)
+        allure.attach.file(filename_pcap, 'ptf_pcap: ' + filename_pcap, allure.attachment_type.PCAP)
 
 def ptf_runner(host, testdir, testname, platform_dir=None, params={},
                platform="remote", qlen=0, relax=True, debug_level="info",
@@ -85,7 +84,9 @@ def ptf_runner(host, testdir, testname, platform_dir=None, params={},
     try:
         result = host.shell(cmd, chdir="/root", module_ignore_errors=module_ignore_errors)
         if log_file:
-            ptf_collect(host, log_file, json.dumps(result, indent=4))
+            ptf_collect(host, log_file)
+        if result:
+            allure.attach(json.dumps(result, indent=4), 'ptf_console_result', allure.attachment_type.TEXT)
         if module_ignore_errors:
             if result["rc"] != 0:
                 return result
@@ -93,6 +94,7 @@ def ptf_runner(host, testdir, testname, platform_dir=None, params={},
         if log_file:
             ptf_collect(host, log_file)
         traceback_msg = traceback.format_exc()
+        allure.attach(traceback_msg, 'ptf_runner_exception_traceback', allure.attachment_type.TEXT)
         logger.error("Exception caught while executing case: {}. Error message: {}"\
             .format(testname, traceback_msg))
         raise Exception
