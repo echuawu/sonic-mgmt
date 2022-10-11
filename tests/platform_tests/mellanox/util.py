50274282482eb1d70f99bd8b2c7e1189c63724e6
Jibin Bao <jbao@nvidia.com>
2022-10-11 18:14:37 +0800

    fix eeprom issue by ihor open
    Fix issue:https://redmine.mellanox.com/issues/3226742. Because for cable SFP/SFP+/SFP28, there is different output, so change case according to the relevant output.
    
    Change-Id: I626aab2940a5cd7a90cf8b3b6618fcf949fba847

--- tests/platform_tests/mellanox/util.py
+++ tests/platform_tests/mellanox/util.py
@@ -188,62 +188,112 @@ def check_sfp_eeprom_info(duthost, sfp_eeprom_info, is_support_dom, show_eeprom_
     """
     logging.info("Check all expected keys exist in sfp info")
     expected_keys = set(["Application Advertisement", "Connector", "Encoding", "Extended Identifier",
-                         "Extended RateSelect Compliance", "Identifier", "Length Cable Assembly(m)",
-                         "Nominal Bit Rate(100Mbs)", "Specification compliance", "Vendor Date Code(YYYY-MM-DD Lot)",
+                         "Extended RateSelect Compliance", "Identifier", "Nominal Bit Rate(100Mbs)",
+                         "Specification compliance", "Vendor Date Code(YYYY-MM-DD Lot)",
                          "Vendor Name", "Vendor OUI", "Vendor PN", "Vendor Rev", "Vendor SN", "ChannelMonitorValues",
                          "ChannelThresholdValues", "ModuleMonitorValues", "ModuleThresholdValues"])
     excluded_keys = set()
     if "202012" in duthost.os_version and show_eeprom_cmd == "sudo sfputil show eeprom -d":
-        excluded_keys = set(["Application Advertisement", "ChannelThresholdValues", "ModuleThresholdValues"])
-        expected_keys = expected_keys - excluded_keys
+        if "TypeOfTransceiver" in sfp_eeprom_info and sfp_eeprom_info["TypeOfTransceiver"] == "SFP/SFP+/SFP28":
+            # There is a bug:https://github.com/sonic-net/sonic-buildimage/issues/12357
+            # So for SFP/SFP+/SFP28, we need do special handle
+            expected_keys = set(["Connector", "EncodingCodes", "ExtIdentOfTypeOfTransceiver",
+                                 "NominalSignallingRate(UnitsOf100Mbd)", "RateIdentifier", "ReceivedPowerMeasurementType",
+                                 "VendorDataCode(YYYY-MM-DD Lot)", "TypeOfTransceiver", "TransceiverCodes",
+                                 "VendorName", "VendorOUI", "VendorPN", "VendorRev", "VendorSN"])
+            is_support_dom = False
+        else:
+            excluded_keys = set(["Application Advertisement", "ChannelThresholdValues", "ModuleThresholdValues"])
+            expected_keys = expected_keys - excluded_keys
+
+    if "Identifier" in sfp_eeprom_info and sfp_eeprom_info["Identifier"] == "SFP/SFP+/SFP28":
+        excluded_keys = excluded_keys | {"ChannelMonitorValues", "ChannelThresholdValues", "ModuleMonitorValues",
+                                         "ModuleThresholdValues"}
+        expected_keys = (expected_keys | {"MonitorData", "ThresholdData"}) - excluded_keys
 
     for key in expected_keys:
         assert key in sfp_eeprom_info, "key {} doesn't exist in {}".format(key, sfp_eeprom_info)
 
+    # For the parameter of Length, there are many different value for different cable,
+    # So here we just check if there is a key including the prefix of Length
+    is_length_key_exist = False
+    for key in sfp_eeprom_info.keys():
+        if key.startswith("Length"):
+            is_length_key_exist = True
+            break
+    assert is_length_key_exist, "Key related to Length doesn't exist in {} ".format(sfp_eeprom_info)
+
     if is_support_dom:
+        pattern_power = "^(?P<digit>-[0-9\.]+|[0-9.]+|-inf)(?P<unit>dBm$)"
+        pattern_bias = "^(?P<digit>-[0-9\.]+|[0-9.]+)(?P<unit>mA$)"
+        pattern_temp = "^(?P<digit>-[0-9\.]+|[0-9.]+)(?P<unit>[Cc]$)"
+        pattern_vcc = "^(?P<digit>-[0-9\.]+|[0-9.]+)(?P<unit>Volts$)"
 
-        if "ChannelThresholdValues" not in excluded_keys:
-            logging.info("Check if ChannelThresholdValues' keys exist and the corresponding value format is correct")
-            expected_channel_threshold_values_keys = ["RxPowerHighAlarm", "RxPowerHighWarning", "RxPowerLowAlarm",
-                                                      "RxPowerLowWarning", "TxBiasHighAlarm", "TxBiasHighWarning",
-                                                      "TxBiasLowAlarm", "TxBiasLowWarning"]
-            for i, key in enumerate(expected_channel_threshold_values_keys):
-                assert key in sfp_eeprom_info["ChannelThresholdValues"], \
-                    "key {} doesn't exist in {}".format(key, sfp_eeprom_info["ChannelThresholdValues"])
-                pattern_digit_unit = "^(?P<digit>-[0-9\.]+|[0-9.]+|-inf)(?P<unit>dBm$)" if i < 4 else "^(?P<digit>-[0-9\.]+|[0-9.]+)(?P<unit>mA$)"
-                assert re.match(pattern_digit_unit, sfp_eeprom_info["ChannelThresholdValues"][key]), \
-                    "Value of {}:{} format is not correct. pattern is {}".format(key,
-                                                                                 sfp_eeprom_info["ChannelThresholdValues"][key],
-                                                                                 pattern_digit_unit)
-        if "ModuleThresholdValues" not in excluded_keys:
-            logging.info("Check if ModuleThresholdValues' keys exist and the corresponding format is correct")
-            expected_module_threshold_values_keys = ["TempHighAlarm", "TempHighWarning", "TempLowAlarm", "TempLowWarning",
-                                                     "VccHighAlarm", "VccHighWarning", "VccLowAlarm", "VccLowWarning"]
-            for i, key in enumerate(expected_module_threshold_values_keys):
-                assert key in sfp_eeprom_info["ModuleThresholdValues"], "key {} doesn't exist in {}".format(key,
-                                                                                                            sfp_eeprom_info["ModuleThresholdValues"])
-                pattern_digit_unit = "^(?P<digit>-[0-9\.]+|[0-9.]+)(?P<unit>[Cc]$)" if i < 4 else "^(?P<digit>-[0-9\.]+|[0-9.]+)(?P<unit>Volts$)"
-                assert re.match(pattern_digit_unit, sfp_eeprom_info["ModuleThresholdValues"][key]), \
-                    "Value of {}:{} format is not correct. pattern is {}".format(key,
-                                                                                 sfp_eeprom_info["ModuleThresholdValues"][key],
-                                                                                 pattern_digit_unit)
+        expected_channel_threshold_values_keys_and_value_pattern = {"RxPowerHighAlarm": pattern_power,
+                                                                    "RxPowerHighWarning": pattern_power,
+                                                                    "RxPowerLowAlarm": pattern_power,
+                                                                    "RxPowerLowWarning": pattern_power,
+                                                                    "TxBiasHighAlarm": pattern_bias,
+                                                                    "TxBiasHighWarning": pattern_bias,
+                                                                    "TxBiasLowAlarm": pattern_bias,
+                                                                    "TxBiasLowWarning": pattern_bias}
+        expected_module_threshold_values_keys_and_value_pattern = {"TempHighAlarm": pattern_temp,
+                                                                   "TempHighWarning": pattern_temp,
+                                                                   "TempLowAlarm": pattern_temp,
+                                                                   "TempLowWarning": pattern_temp,
+                                                                   "VccHighAlarm": pattern_vcc,
+                                                                   "VccHighWarning": pattern_vcc,
+                                                                   "VccLowAlarm": pattern_vcc,
+                                                                   "VccLowWarning": pattern_vcc}
+        expected_module_monitor_values_keys_and_pattern = {"Temperature": pattern_temp, "Vcc": pattern_vcc}
 
-        logging.info("Check if ChannelMonitorValues's value format is correct")
-        for k, v in sfp_eeprom_info["ChannelMonitorValues"].items():
-            pattern_digit_unit = "^(?P<digit>-[0-9\.]+|[0-9.]+|-inf)(?P<unit>dBm$)" if "Power" in k else "^(?P<digit>-[0-9\.]+|[0-9.]+)(?P<unit>mA$)"
-            assert re.match(pattern_digit_unit, v), \
-                "Value of {}:{} format is not correct. pattern is {}".format(k, v, pattern_digit_unit)
+        if sfp_eeprom_info["Identifier"] == "SFP/SFP+/SFP28":
+            expected_monitor_data_keys_and_pattern = {"RXPower": pattern_power, "TXBias": pattern_bias,
+                                                      "TXPower": pattern_power, "Temperature": pattern_temp,
+                                                      "Vcc": pattern_vcc}
+            expected_threshold_data_keys_and_pattern = {"TxPowerHighAlarm": pattern_power,
+                                                        "TxPowerHighWarning": pattern_power,
+                                                        "TxPowerLowAlarm": pattern_power,
+                                                        "TxPowerLowWarning": pattern_power}
+            expected_threshold_data_keys_and_pattern.update(expected_channel_threshold_values_keys_and_value_pattern)
+            expected_threshold_data_keys_and_pattern.update(expected_module_threshold_values_keys_and_value_pattern)
 
-        logging.info("Check ModuleMonitorValues keys exist and the corresponding value format is correct")
-        expected_module_monitor_values_keys = ["Temperature", "Vcc"]
-        for i, key in enumerate(expected_module_monitor_values_keys):
-            assert key in sfp_eeprom_info["ModuleMonitorValues"], "key {} doesn't exist in {}".format(key,
-                                                                                                      sfp_eeprom_info["ModuleThresholdValues"])
-            pattern_digit_unit = "^(?P<digit>-[0-9\.]+|[0-9.]+)(?P<unit>[Cc]$)" if i < 1 else "^(?P<digit>-[0-9\.]+|[0-9.]+)(?P<unit>Volts$)"
-            assert re.match(pattern_digit_unit, sfp_eeprom_info["ModuleMonitorValues"][key]), \
-                "Value of {}:{} format is not correct. pattern is {}".format(key,
-                                                                             sfp_eeprom_info["ModuleMonitorValues"][key],
-                                                                             pattern_digit_unit)
+            logging.info("Check if MonitorData's keys exist and the corresponding value format is correct")
+            check_dom_monitor_key_and_data_format(expected_monitor_data_keys_and_pattern,
+                                                  sfp_eeprom_info["MonitorData"])
+
+            logging.info("check if ThresholdData's keys exist and the corresponding value format is correct")
+            check_dom_monitor_key_and_data_format(expected_threshold_data_keys_and_pattern,
+                                                  sfp_eeprom_info["ThresholdData"])
+
+        else:
+            if "ChannelThresholdValues" not in excluded_keys:
+                logging.info(
+                    "Check if ChannelThresholdValues' keys exist and the corresponding value format is correct")
+                check_dom_monitor_key_and_data_format(expected_channel_threshold_values_keys_and_value_pattern,
+                                                      sfp_eeprom_info["ChannelThresholdValues"])
+
+            if "ModuleThresholdValues" not in excluded_keys:
+                logging.info("Check if ModuleThresholdValues' keys exist and the corresponding format is correct")
+                check_dom_monitor_key_and_data_format(expected_module_threshold_values_keys_and_value_pattern,
+                                                      sfp_eeprom_info["ModuleThresholdValues"])
+
+            logging.info("Check if ChannelMonitorValues's value format is correct")
+            for k, v in sfp_eeprom_info["ChannelMonitorValues"].items():
+                pattern = pattern_power if "Power" in k else pattern_bias
+                assert re.match(pattern, v), "Value of {}:{} format is not correct. pattern is {}".format(k, v, pattern)
+
+            logging.info("Check ModuleMonitorValues keys exist and the corresponding value format is correct")
+            check_dom_monitor_key_and_data_format(expected_module_monitor_values_keys_and_pattern,
+                                                  sfp_eeprom_info["ModuleMonitorValues"])
+
+
+def check_dom_monitor_key_and_data_format(expected_keys_and_pattern_dict, dom_monitor_data):
+    for key, pattern in expected_keys_and_pattern_dict.items():
+        assert key in dom_monitor_data, "Key {} doesn't exist in {}".format(key, dom_monitor_data)
+        assert re.match(pattern, dom_monitor_data[key]), \
+            "Value of {}:{} format is not correct. pattern is {}".format(
+                key, dom_monitor_data[key], pattern)
 
 
 def is_support_dom(duthost, port_index):
