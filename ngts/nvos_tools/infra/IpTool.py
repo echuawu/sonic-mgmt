53c293f3640016d51dfce33d487f0ffe92446a89
Sviatoslav Demniak <sviatoslavd@mellanox.com>
2022-11-07 09:38:04 +0200

    changed generate  ipv4 func due to issue 3242156 to not use our lab ip's
    Change-Id: Icb054ac9a426cd599cd2eb60bf6820618f5ef46e

--- ngts/nvos_tools/infra/IpTool.py
+++ ngts/nvos_tools/infra/IpTool.py
@@ -10,16 +10,17 @@ class IpTool:
     @staticmethod
     def select_random_ipv4_address():
         """
-        generate random ip, no multicast, no loopback
+        generate random ip, no multicast, no loopback, changed func to generate ip, not related to NVOS  Lab ip's,
+        details in https://redmine.mellanox.com/issues/3242156
         :return: IP as a string (all the fields are 1-254)
         """
         with allure.step('Select random ipv4_address'):
             result_obj = ResultObj(True, "")
-            msb_list = list(range(1, 127)) + list(range(128, 200))
+            msb_list = list(range(14, 127)) + list(range(128, 200))
             msb = random.choice(msb_list)
             result_obj.returned_value = ('{0}.{1}.{2}.{3}/{4}'.format(msb, random.randint(1, 254),
                                                                       random.randint(1, 254), random.randint(1, 254),
-                                                                      random.randint(1, 24)))
+                                                                      random.randint(8, 32)))
             return result_obj
 
     @staticmethod
@@ -34,5 +35,5 @@ class IpTool:
                                              randint(IpConsts.MIN_IPV6_GROUP_VALUE, IpConsts.MAX_IPV6_GROUP_VALUE))
                                for _ in range(8))
 
-            result_obj.returned_value = ip_addr + ('/{0}'.format(random.randint(1, 24)))
+            result_obj.returned_value = ip_addr + ('/{0}'.format(random.randint(1, 128)))
             return result_obj
