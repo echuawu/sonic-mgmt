ff99aa6a97a7d3713ebc2e65e47b3ca6c0d744b8
Cong Hou <congh@nvidia.com>
2022-12-14 21:02:36 +0800

    fix test test_downstream_ecmp_nexthops
    Ignore IP flags in the mask of exp_tunnel_pkt, for the don't fragment flag is set by the switch in the outer IP header.
    
    
    Change-Id: I53df6a83198153a6ef6d7a532b0ad6e9fdd15284

--- tests/common/dualtor/dual_tor_utils.py
+++ tests/common/dualtor/dual_tor_utils.py
@@ -856,6 +856,7 @@ def generate_hashed_packet_to_server(ptfadapter, duthost, hash_key, target_serve
         exp_tunnel_pkt.set_do_not_care_scapy(IP, "id") # since src and dst changed, ID would change too
         exp_tunnel_pkt.set_do_not_care_scapy(IP, "ttl") # ttl in outer packet is set to 255
         exp_tunnel_pkt.set_do_not_care_scapy(IP, "chksum") # checksum would differ as the IP header is not the same
+        exp_tunnel_pkt.set_do_not_care_scapy(IP, "flags")  # the flag is '0x2, Don't fragment' in outer IP header
 
         return send_pkt, exp_pkt, exp_tunnel_pkt
 
@@ -897,6 +898,7 @@ def generate_hashed_packet_to_server(ptfadapter, duthost, hash_key, target_serve
         exp_tunnel_pkt.set_do_not_care_scapy(IP, "id")
         exp_tunnel_pkt.set_do_not_care_scapy(IP, "ttl")
         exp_tunnel_pkt.set_do_not_care_scapy(IP, "chksum")
+        exp_tunnel_pkt.set_do_not_care_scapy(IP, "flags")
 
         return send_pkt, exp_pkt, exp_tunnel_pkt
 
