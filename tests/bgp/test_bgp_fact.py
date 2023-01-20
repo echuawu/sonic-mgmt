eef27b926cbedc9a9c731f9f246cec6bd9ca0b41
Petro Pikh <petrop@nvidia.com>
2023-01-20 11:05:32 +0200

    Removed temporary workarounds for DPU platform from test_bgp_facts
    - Removed ARP workaround
    - Removed skip for IPv6 validation
    
    Change-Id: I8271439dd12e130344ab95a7cdbdad992ffa5da5

--- tests/bgp/test_bgp_fact.py
+++ tests/bgp/test_bgp_fact.py
@@ -1,75 +1,9 @@
 import pytest
 
-from tests.common.utilities import wait_until
-
 pytestmark = [
     pytest.mark.topology('any'),
     pytest.mark.device_type('vs')
 ]
-
-
-@pytest.fixture(scope='module', autouse=True)
-def tmp_static_arp_workaround(tbinfo, nbrhosts, duthost):
-
-    eos_peers_config = {}
-    sonic_peers_config = []
-
-    if 'Nvidia-MBF2H536C' in duthost.facts["hwsku"]:
-        mg_facts = duthost.get_extended_minigraph_facts(tbinfo)
-        config_facts = duthost.get_running_config_facts()
-
-        # Collect info about peers IP, MAC, ifaces
-        for iface_info in mg_facts['minigraph_interfaces']:
-            sonic_local_ip_addr = iface_info['addr']
-            sonic_local_port = config_facts['port_alias_to_name_map'][iface_info['attachto']]
-            sonic_local_port_alias = iface_info['attachto']
-            sonic_local_port_mac = duthost.get_dut_iface_mac(sonic_local_port)
-
-            eos_peer_ip_addr = iface_info['peer_addr']
-            eos_peer_name = mg_facts['minigraph_neighbors'][sonic_local_port_alias]['name']
-            eos_peer_port = mg_facts['minigraph_neighbors'][sonic_local_port_alias]['port']
-
-            peer_config_dict = {'port': eos_peer_port, 'ip': sonic_local_ip_addr, 'mac': sonic_local_port_mac}
-            eos_peers_config[eos_peer_name] = peer_config_dict
-
-            eos_iface_mac = nbrhosts[eos_peer_name]['host'].get_dut_iface_mac(eos_peer_port)
-            sonic_peers_config.append({'port': sonic_local_port, 'ip': eos_peer_ip_addr, 'mac': eos_iface_mac})
-
-        # Configure peers static ARP
-        for host_name, nbr in nbrhosts.items():
-            config = eos_peers_config[host_name]
-            ip = config['ip']
-            if ':' in ip:
-                nbr['host'].eos_config(lines=['ipv6 neighbor {} {} {}'.format(ip, config['port'], config['mac'])])
-            else:
-                nbr['host'].eos_config(lines=['arp {} {} arpa'.format(ip, config['mac'])])
-
-        # Configure SONiC static ARP
-        for item in sonic_peers_config:
-            duthost.shell('sudo ip neigh add {} dev {} lladdr {}'.format(item['ip'], item['port'], item['mac']),
-                          module_ignore_errors=True)
-            duthost.shell('sudo ip neigh change {} dev {} lladdr {}'.format(item['ip'], item['port'], item['mac']),
-                          module_ignore_errors=True)
-
-        # Wait untill BGP established
-        bgp_neighbors = config_facts.get('BGP_NEIGHBOR', {})
-        wait_until(300, 10, 0, duthost.check_bgp_session_state, bgp_neighbors.keys())
-
-    yield
-
-    if 'Nvidia-MBF2H536C' in duthost.facts["hwsku"]:
-        # Remove SONiC static ARP
-        for item in sonic_peers_config:
-            duthost.shell('sudo ip neigh flush dev {}'.format(item['port']), module_ignore_errors=True)
-
-        # Remove peers static ARP
-        for host_name, nbr in nbrhosts.items():
-            config = eos_peers_config[host_name]
-            ip = config['ip']
-            if ':' in ip:
-                nbr['host'].eos_config(lines=['no ipv6 neighbor {} {}'.format(ip, config['port'])])
-            else:
-                nbr['host'].eos_config(lines=['no arp {}'.format(ip)])
 
 
 def test_bgp_facts(duthosts, enum_frontend_dut_hostname, enum_asic_index):
@@ -83,9 +17,6 @@ def test_bgp_facts(duthosts, enum_frontend_dut_hostname, enum_asic_index):
 
     sonic_db_cmd = "sonic-db-cli {}".format("-n " + namespace if namespace else "")
     for k, v in bgp_facts['bgp_neighbors'].items():
-        # TODO: remove this skip of ipv6 when the 3264845 will be resolved
-        if 'nvda_bf' in duthost.facts['platform'] and ':' in k:
-            continue
         # Verify bgp sessions are established
         assert v['state'] == 'established'
         # Verify local ASNs in bgp sessions
@@ -102,9 +33,6 @@ def test_bgp_facts(duthosts, enum_frontend_dut_hostname, enum_asic_index):
     # In VoQ Chassis, we would have BGP_VOQ_CHASSIS_NEIGHBOR as well.
     nbrs_in_cfg_facts.update(config_facts.get('BGP_VOQ_CHASSIS_NEIGHBOR', {}))
     for k, v in nbrs_in_cfg_facts.items():
-        # TODO: remove this skip of ipv6 when the 3264845 will be resolved
-        if 'nvda_bf' in duthost.facts['platform'] and ':' in k:
-            continue
         # Compare the bgp neighbors name with config db bgp neighbors name
         assert v['name'] == bgp_facts['bgp_neighbors'][k]['description']
         # Compare the bgp neighbors ASN with config db
