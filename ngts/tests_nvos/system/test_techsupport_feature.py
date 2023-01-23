457f4170aec8df26cddbbbef0cabdec51580b756
rowaida mahameed <rmahameed@nvidia.com>
2023-01-23 14:16:27 +0200

    NVOS - add upload/delete tech-support files tests and update the show command format
    Change-Id: Ia2c052eb24eb12386ade2e04ed7f90dceca3ad61

--- ngts/tests_nvos/system/test_techsupport_feature.py
+++ ngts/tests_nvos/system/test_techsupport_feature.py
@@ -19,11 +19,11 @@ def test_techsupport_folder_name(engines):
     """
     with allure.step('Run nv action generate system tech-support and validate the tech-support name'):
         system = System(None)
-        output_dictionary_before = OutputParsingTool.parse_show_system_techsupport_output_to_dictionary(
+        output_dictionary_before = OutputParsingTool.parse_show_system_techsupport_output_to_list(
             system.techsupport.show()).get_returned_value()
         tech_support_folder = system.techsupport.action_generate()
         validate_techsupport_folder_name(system, tech_support_folder)
-        output_dictionary_after = OutputParsingTool.parse_show_system_techsupport_output_to_dictionary(
+        output_dictionary_after = OutputParsingTool.parse_show_system_techsupport_output_to_list(
             system.techsupport.show()).get_returned_value()
 
         cleanup_techsupport(engines.dut, output_dictionary_before, output_dictionary_after)
