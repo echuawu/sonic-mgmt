d4a216bfcfb7c461b36309bf0a85213e453a2be7
Azmy Ali <azmya@nvidia.com>
2023-01-31 14:42:09 +0200

    updating docker image to devts version contating  the detach of unapplied configuration before running  restore password for password prompt
    Change-Id: Ic464c92912fee10d29ed92c6156685f2f5a9c220

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -362,7 +362,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.172'}
+    default_list = {'docker-ngts': '1.2.173'}
     return default_list.get(docker_name, latest)
 
 
