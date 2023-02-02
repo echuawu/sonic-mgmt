9734e0df5dc8b98c095f5738ded409d98f8f2eea
Batel Shpigel <bshpigel@nvidia.com>
2023-02-02 12:26:53 +0200

    add multi asic .cases and .db \n add docker and services dependendcy to .cases file \n change test_save_after_reboot test
    Change-Id: Ibc935f93320d44e02943f25100942b7f06cb87b1

--- ngts/nvos_tools/system/Reboot.py
+++ ngts/nvos_tools/system/Reboot.py
@@ -1,4 +1,6 @@
 import allure
+import logging
+import time
 from ngts.nvos_tools.infra.BaseComponent import BaseComponent
 from ngts.cli_wrappers.nvue.nvue_system_clis import NvueSystemCli
 from ngts.cli_wrappers.nvue.nvue_general_clis import NvueGeneralCli
@@ -6,6 +8,8 @@ from ngts.cli_wrappers.openapi.openapi_system_clis import OpenApiSystemCli
 from ngts.nvos_tools.infra.NvosTestToolkit import TestToolkit
 from ngts.nvos_tools.infra.SendCommandTool import SendCommandTool
 from ngts.nvos_constants.constants_nvos import ApiType
+
+logger = logging.getLogger()
 
 
 class Reboot(BaseComponent):
@@ -23,6 +27,20 @@ class Reboot(BaseComponent):
         with allure.step('Execute action for {resource_path}'.format(resource_path=self.get_resource_path())):
             if not engine:
                 engine = TestToolkit.engines.dut
-            return SendCommandTool.execute_command(self.api_obj[TestToolkit.tested_api].action_reboot,
-                                                   engine,
-                                                   self.get_resource_path().replace('/reboot', ' '), params)
+
+            start_time = time.time()
+            res_obj = SendCommandTool.execute_command(self.api_obj[TestToolkit.tested_api].action_reboot,
+                                                      engine,
+                                                      self.get_resource_path().replace('/reboot', ' '), params)
+            end_time = time.time()
+            duration = end_time - start_time
+
+            with allure.step("Reboot takes: {} seconds".format(duration)):
+                logger.info("Reboot takes: {} seconds".format(duration))
+
+            NvueGeneralCli.wait_for_nvos_to_become_functional(engine)
+            end_time = time.time()
+            duration = end_time - start_time
+            with allure.step("Reboot till system is functional takes: {} seconds".format(duration)):
+                logger.info("Reboot till system is functional takes: {} seconds".format(duration))
+            return res_obj
