df7bd17693a1a9a8c7c4426176a96aef162ea752
Batel Shpigel <bshpigel@nvidia.com>
2023-02-15 11:44:06 +0200

    fix test fw install
    Change-Id: I0845bb239933b1f91bcdf048e13e0bbe75e12085

--- ngts/nvos_tools/system/Asic.py
+++ ngts/nvos_tools/system/Asic.py
@@ -1,7 +1,13 @@
 from ngts.nvos_tools.infra.BaseComponent import BaseComponent
-from ngts.nvos_constants.constants_nvos import ApiType
+from ngts.nvos_constants.constants_nvos import ApiType, ActionConsts
 from ngts.cli_wrappers.nvue.nvue_system_clis import NvueSystemCli
 from ngts.cli_wrappers.openapi.openapi_system_clis import OpenApiSystemCli
+from ngts.nvos_tools.infra.SendCommandTool import SendCommandTool
+from ngts.nvos_tools.infra.NvosTestToolkit import TestToolkit
+import allure
+import logging
+
+logger = logging.getLogger()
 
 
 class Asic(BaseComponent):
@@ -22,3 +28,10 @@ class Asic(BaseComponent):
 
     def set(self, op_param_name="", op_param_value=""):
         raise Exception("set is not implemented for /asic/{asic_component}")
+
+    def action_install(self, fw_file_path):
+        with allure.step("Install system firmware asic: '{path}'".format(path=fw_file_path)):
+            logger.info("Install system firmware asic: '{path}'".format(path=fw_file_path))
+            return SendCommandTool.execute_command(self.api_obj[TestToolkit.tested_api].action_firmware_install,
+                                                   TestToolkit.engines.dut,
+                                                   self.get_resource_path().replace("/", " "), fw_file_path).get_returned_value()
