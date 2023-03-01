b87f4f0219157283d3dce45009815ad46623113f
Cong Hou <congh@nvidia.com>
2023-03-01 16:39:10 +0800

    Add Mellanox dualtor hwskus in the ansible variables
    Replace the temp change of the dualtor hwskus by a permanent one.
    
    Change-Id: I883ea4a36be0a38aeb4cad876c138f79ae7297fa

--- tests/dualtor/conftest.py
+++ tests/dualtor/conftest.py
@@ -66,7 +66,7 @@ def pytest_addoption(parser):
 def common_setup_teardown(rand_selected_dut, request, tbinfo, vmhost):
     # Skip dualtor test cases on unsupported platform
     supported_platforms = \
-        ['broadcom_td3_hwskus', 'broadcom_th2_hwskus', 'cisco_hwskus', 'mellanox_spc1_hwskus', 'mellanox_spc3_hwskus']
+        ['broadcom_td3_hwskus', 'broadcom_th2_hwskus', 'cisco_hwskus', 'mellanox_dualtor_hwskus']
     hostvars = get_host_visible_vars(rand_selected_dut.host.options['inventory'], rand_selected_dut.hostname)
     hwsku = rand_selected_dut.facts['hwsku']
     skip = True
