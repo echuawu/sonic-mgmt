ad866cfd982866affef7aeabee8dbd373346fd44
Asaf Lavi <asafl@nvidia.com>
2023-03-15 08:57:23 +0200

    Update NGCI version from 5.0 to 5.0.2
    Signed-off-by: Asaf Lavi <asafl@nvidia.com>
    Change-Id: I7a79ce3afa99a8b18b0d9805348b2f58de749194

--- build/common/mgmt_tools.groovy
+++ build/common/mgmt_tools.groovy
@@ -3,6 +3,7 @@ package com.mellanox.jenkins
 def get_sonic_lastrc_version(target_branch) {
     //Check for lastrc
     try {
+        print "Getting lastrc SONiC version"
         def version_path = env.VERSION_DIRECTORY
         def lastrc = NGCITools().ciTools.run_sh_return_output("readlink ${version_path}/${target_branch}-lastrc-internal-sonic-mellanox.bin")
         if (lastrc.contains("_Public")) {
@@ -20,6 +21,7 @@ def get_sonic_lastrc_version(target_branch) {
 def get_nvos_lastrc_version(target_branch) {
     //Check for lastrc
     try {
+        print "Getting lastrc NVOS version"
         def version_path = env.NVOS_VERSION_DIRECTORY
         def lastrc = NGCITools().ciTools.run_sh_return_output("readlink ${version_path}/lastrc_${target_branch}")
         def lastrc_version = lastrc.replace("${version_path}", "").replace("/amd64/", "").replace("/", "")
