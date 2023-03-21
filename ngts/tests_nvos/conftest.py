55836f42b0ecaa971c6d9b7cc6bc5d17d7e5ca83
Batel Shpigel <bshpigel@nvidia.com>
2023-03-21 11:37:44 +0200

    cli coverage will run only during mars session and not on local runs
    Change-Id: I6562d0c24d70dd7d292f94c3e9bbcd5c7c1463d0
    (cherry picked from commit c20c42ff26ef7adfe81a14191732b2049de07a20)

--- ngts/tests_nvos/conftest.py
+++ ngts/tests_nvos/conftest.py
@@ -224,7 +224,7 @@ def save_results_and_clear_after_test(item):
     try:
         logging.info(' ---------------- The test completed successfully ---------------- ')
         if TestToolkit.tested_api == ApiType.NVUE and os.path.exists('/auto/sw/tools/comet/nvos/') \
-                and 'no_cli_coverage_run' not in markers and not pytest.is_sanitizer:
+                and 'no_cli_coverage_run' not in markers and not pytest.is_sanitizer and pytest.is_mars_run:
             logging.info("API type is NVUE and is it not a sanitizer version, so CLI coverage script will run")
             NVUECliCoverage.run(item, pytest.s_time)
     except KeyboardInterrupt:
