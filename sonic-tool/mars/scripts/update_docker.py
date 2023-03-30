70e8f80255fd8cde7e595e31a79b79f3e9536ef0
AntonHryshchuk <antonh@nvidia.com>
2023-03-31 00:54:55 +0300

    Add new setup - moose-42
    Change-Id: Iabecd48b22287986b71a3f5e31757977448f7f28
    Signed-off-by: AntonHryshchuk <antonh@nvidia.com>

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -362,7 +362,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.198'}
+    default_list = {'docker-ngts': '1.2.201'}
     return default_list.get(docker_name, latest)
 
 
