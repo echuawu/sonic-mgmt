8dc65144e43c9cdec5e07945451580160d648c2c
akd@nvidia.com <akd@nvidia.com>
2023-04-19 15:07:49 +0300

    Scrum Task #3432856
    Fix for NV OS: interface description: after "nv unset interface" command the description is not deleted
    Change-Id: I0a0aad1e643e55f61588e012a9d7a0fb9a788942

---
+++ ngts/tests_nvos/interfaces/test_ib_set_interface_description.py
@@ -0,0 +1,73 @@
+import logging
+import allure
+import pytest
+
+from ngts.nvos_tools.infra.Tools import Tools
+from ngts.nvos_tools.ib.InterfaceConfiguration.Port import Port
+from ngts.nvos_tools.infra.NvosTestToolkit import TestToolkit
+from ngts.nvos_tools.ib.InterfaceConfiguration.nvos_consts import NvosConsts
+from ngts.cli_wrappers.nvue.nvue_interface_show_clis import OutputFormat
+from ngts.nvos_tools.ib.opensm.OpenSmTool import OpenSmTool
+from ngts.nvos_constants.constants_nvos import ApiType
+
+logger = logging.getLogger()
+
+
+@pytest.mark.ib_interfaces
+@pytest.mark.ib
+def test_ib_set_interface_description(engines):
+    """
+    Set and unset interface description and verify the show interface output using
+    command: nv show interface <name>
+
+    flow:
+    1. Select a random port (status of which is up)
+    2. Run 'nv show interface <name>' on selected port
+    3. Validate if description is empty
+    4. Run 'nv set interface <name> description abcd'
+    5. Apply config
+    6. Run 'nv show interface <name>' on selected port
+    7. Validate interface description is updated
+    8. Run 'nv unset interface <name>'
+    9. Apply config
+    10. Run 'nv show interface <name>'
+    11. Validate if description is cleared
+    """
+    DESCRIPTION_EMPTY = ""
+    DESCRIPTION_ABCD = "abcd"
+    selected_port = Tools.RandomizationTool.select_random_port().get_returned_value()
+    selected_port.update_output_dictionary()
+
+    TestToolkit.update_tested_ports([selected_port])
+
+    with allure.step('Run show command on selected port and verify that description field is empty'):
+        validate_interface_description_field(selected_port, DESCRIPTION_EMPTY, True)
+
+    with allure.step('Run show command on selected port and verify that description field is set'):
+        selected_port.ib_interface.description.set(DESCRIPTION_ABCD, apply=True).verify_result()
+        selected_port.update_output_dictionary()
+        validate_interface_description_field(selected_port, DESCRIPTION_ABCD, True)
+
+    with allure.step('Run show command on selected port and verify that description field is cleared back'):
+        selected_port.ib_interface.description.unset(apply=True).verify_result()
+        selected_port.update_output_dictionary()
+        validate_interface_description_field(selected_port, DESCRIPTION_EMPTY, True)
+
+
+def validate_interface_description_field(selected_port, description_value, should_be_equal):
+    with allure.step('Check that interface description field matches the expected value'):
+        logging.info('Check that interface description field matches the expected value')
+        output_dictionary = selected_port.show_output_dictionary
+        field_name = selected_port.ib_interface.description.label
+        Tools.ValidationTool.verify_field_value_in_output(output_dictionary,
+                                                          field_name, description_value).verify_result()
+
+
+# ------------ Open API tests -----------------
+
+@pytest.mark.openapi
+@pytest.mark.ib_interfaces
+@pytest.mark.ib
+def test_ib_set_interface_description_openapi(engines):
+    TestToolkit.tested_api = ApiType.OPENAPI
+    test_ib_set_interface_description(engines)
