61e79aaed21b36e54fcf836c5297b3d62051c458
Alon Navarro <alonn@mtl-vdi-684.wap.labs.mlnx>
2023-05-26 20:44:05 +0300

    add caller file name and line num to the allure step logging
    Change-Id: Ib584c056340b70b6eb0ae2167bcef07165ecdbf6

--- ngts/tools/test_utils/allure_utils.py
+++ ngts/tools/test_utils/allure_utils.py
@@ -1,7 +1,10 @@
+import os
 from contextlib import contextmanager
-
+import inspect
 import allure
 import logging
+
+logger = logging.getLogger(__name__)
 
 
 @contextmanager
@@ -11,9 +14,14 @@ def step(step_msg):
         Context manager that wraps allure step context and a log with the same message
     @param step_msg: The desired step message
     """
+    caller_frame = inspect.currentframe().f_back.f_back
+    caller_file = inspect.getframeinfo(caller_frame).filename
+    lineno = caller_frame.f_lineno
+    filename = os.path.basename(caller_file)
+
     with allure.step(step_msg) as allure_step_context:
-        logging.info(f'Step start: {step_msg}')
+        logging.info(f'({filename}:{lineno}) Step start: {step_msg}')
         try:
             yield allure_step_context
         finally:
-            logging.info(f'Step end: {step_msg}')
+            logging.info(f'({filename}:{lineno}) Step end: {step_msg}')
