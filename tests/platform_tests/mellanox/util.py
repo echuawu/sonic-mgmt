35a472d9266979fed357631e8648725077d5e423
Jibin Bao <jbao@nvidia.com>
2023-06-12 14:12:24 +0300

    Revert "update sfp eeprom case"
    This reverts commit dcdccf64971e9d03818c28c21d638d53bd065a79.
    
    Reason for revert: Design change due to https://redmine.mellanox.com/issues/3477221
    
    Change-Id: Ie5a6cebbe55b2a73138bf19f2033e36fa13f672f

--- tests/platform_tests/mellanox/util.py
+++ tests/platform_tests/mellanox/util.py
@@ -215,8 +215,7 @@ def check_sfp_eeprom_info(duthost, sfp_eeprom_info, is_support_dom, show_eeprom_
                 ["Application Advertisement", "ChannelThresholdValues", "ModuleThresholdValues"])
             expected_keys = expected_keys - excluded_keys
 
-    sfp_type_for_excluded_monitor_threshold_key = ["SFP/SFP+/SFP28", "OSFP 8X Pluggable Transceiver"]
-    if "Identifier" in sfp_eeprom_info and sfp_eeprom_info["Identifier"] in sfp_type_for_excluded_monitor_threshold_key:
+    if "Identifier" in sfp_eeprom_info and sfp_eeprom_info["Identifier"] == "SFP/SFP+/SFP28":
         excluded_keys = excluded_keys | {"ChannelMonitorValues", "ChannelThresholdValues", "ModuleMonitorValues",
                                          "ModuleThresholdValues"}
         expected_keys = (expected_keys | {
