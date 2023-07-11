d210d2e3d0215c2f5c0429b1cdff3ed7db987e5c
rowaida mahameed <rmahameed@nvidia.com>
2023-07-11 12:52:50 +0300

    NVOS - 1. added system documents files test to ignore list 2. added system ready cli test to regression 3. updated system ready feature test using ping instead of sleep
    Change-Id: I14e77d16e0fa6a3d877f9c473eff45188d53406e

--- ngts/nvos_constants/constants_nvos.py
+++ ngts/nvos_constants/constants_nvos.py
@@ -297,7 +297,7 @@ class SystemConsts:
 
 
 class DocumentsConsts:
-    MIN_FILES_SIZE = 2000
+    MIN_FILES_SIZE = 30000
     TYPE_EULA = 'EULA'
     TYPE_USER_MANUAL = 'User manual'
     TYPE_OPEN_SOURCE_LICENSES = 'Open source licenses'
