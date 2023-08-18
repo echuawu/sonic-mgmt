891fdc6b074ca74c94a2ed9fbf30d9b52f7b292d
Nana He <nhe@nvidia.com>
2023-08-18 18:56:54 +0800

    Align the psu capabilities
    1. Update the psu capabilities used in the test for moose
    2. Add the psu volt in capability
    
    Change-Id: I3cef68933de216790be4cb0638f00f5b18acefa5

--- tests/common/mellanox_data.py
+++ tests/common/mellanox_data.py
@@ -11,7 +11,7 @@ SWITCH_HWSKUS = SPC1_HWSKUS + SPC2_HWSKUS + SPC3_HWSKUS + SPC4_HWSKUS
 
 PSU_CAPABILITIES = [
     ['psu{}_curr', 'psu{}_curr_in', 'psu{}_power', 'psu{}_power_in', 'psu{}_volt', 'psu{}_volt_in', 'psu{}_volt_out'],
-    ['psu{}_curr', 'psu{}_curr_in', 'psu{}_power', 'psu{}_power_in', 'psu{}_volt', 'psu{}_volt_out2']
+    ['psu{}_curr', 'psu{}_curr_in', 'psu{}_power', 'psu{}_power_in', 'psu{}_volt', 'psu{}_volt_in', 'psu{}_volt_out2']
 ]
 RESPINED_PLATFORM = ['x86_64-mlnx_msn4700-r0', 'x86_64-mlnx_msn4410-r0', 'x86_64-mlnx_msn4600c-r0', \
                      'x86_64-mlnx_msn3700-r0', 'x86_64-mlnx_msn3700c-r0', 'x86_64-mlnx_msn2700-r0']
@@ -30,7 +30,7 @@ SWITCH_MODELS = {
         "psus": {
             "number": 2,
             "hot_swappable": True,
-            "capabilities": PSU_CAPABILITIES[0]
+            "capabilities": PSU_CAPABILITIES[1]
         },
         "cpu_pack": {
             "number": 1
