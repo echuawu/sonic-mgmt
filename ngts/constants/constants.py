8e3263f0ee6a6a4da79181497297b240407b368e
Batel Shpigel <bshpigel@nvidia.com>
2023-10-23 16:08:47 +0300

    update gnmi version
    Change-Id: I09c2cf8854a522f39361af759651db27f029f254

--- ngts/constants/constants.py
+++ ngts/constants/constants.py
@@ -1184,7 +1184,7 @@ class GnmiConsts:
     GNMI_IS_RUNNING = 'yes'
     GNMI_IS_NOT_RUNNING = 'no'
     GNMI_VERSION_FIELD = 'version'
-    GNMI_VERSION = '1.0.2'
+    GNMI_VERSION = '4.8.0-5'
     GNMI_DEFAULT_PORT = '9339'
     SLEEP_TIME_FOR_UPDATE = 35
     REDIS_CMD_KEY = 'redis_cmd'
