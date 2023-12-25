aea3ca5e13ff71244c349f4e84553adc44d493a4
Batel Shpigel <bshpigel@nvidia.com>
2023-12-25 17:28:48 +0200

    fix error_to_regex function
    before the change it ruin hexadecimal regex, 
    for example, changed 0x7fd38b276130 to \d+x[\[\d+a-fA-F]++[\d+a-fA-F]+-[\d+a-fA-F]+-[\d+a-fA-F]+]+ instead of 0x[\\d+a-fA-F]+
    
    Change-Id: I2ac56199b11984fe9ecedf835c8847a2b45027f5

--- ngts/helpers/bug_handler/bug_handler_helper.py
+++ ngts/helpers/bug_handler/bug_handler_helper.py
@@ -390,7 +390,7 @@ def error_to_regex(error_string):
                           r" [A-Za-z]{3} \\\\d{4}-\\\\d{2}-\\\\d{2} \\\\d{2}:\\\\d{2}:\\\\d{2} [A-Z]{3} ", error_string)
     # -- Replaces a hex number with the hex regular expression
     error_string = re.sub(r"0x[0-9a-fA-F]+", r"0x[\\\\d+a-fA-F]+", error_string)
-    error_string = re.sub(r"\b[0-9a-fA-F]+\b", r"[\\\\d+a-fA-F]+", error_string)
+    error_string = re.sub(r"\b[0-9a-fA-F]{3,}\b", r"[\\\\d+a-fA-F]+", error_string)
     # -- Replaces any remaining digits with the digit regular expression
     error_string = re.sub(r"\d+", r"\\\\d+", error_string)
     error_string = re.sub(r'"', r'\"', error_string)
