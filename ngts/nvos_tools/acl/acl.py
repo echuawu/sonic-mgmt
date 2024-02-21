308be23280224194f0c81602ff468a4153c1bd74
Batel Shpigel <bshpigel@nvidia.com>
2024-02-21 18:13:35 +0200

    change the ACL and rule classes
    Change-Id: I1d3a7d9eda21e314631d9409bad42a021298e930

--- ngts/nvos_tools/acl/acl.py
+++ ngts/nvos_tools/acl/acl.py
@@ -1,11 +1,13 @@
 import logging
 
+from typing import Dict
 from ngts.cli_wrappers.nvue.nvue_base_clis import NvueBaseCli
 from ngts.cli_wrappers.openapi.openapi_base_clis import OpenApiBaseCli
 from ngts.nvos_tools.infra.BaseComponent import BaseComponent
 from ngts.nvos_tools.infra.NvosTestToolkit import TestToolkit
 from ngts.nvos_tools.acl.rule import Rule
 from ngts.nvos_constants.constants_nvos import ApiType
+from ngts.nvos_tools.infra.DefaultDict import DefaultDict
 
 
 logger = logging.getLogger()
@@ -14,36 +16,14 @@ logger = logging.getLogger()
 class Acl(BaseComponent):
 
     def __init__(self, parent_obj=None):
-        BaseComponent.__init__(self, parent=parent_obj,
-                               api={ApiType.NVUE: NvueBaseCli, ApiType.OPENAPI: OpenApiBaseCli}, path='/acl')
-        self.acl_dict = {}
-
-    def set(self, acl_id, expected_str='', apply=False, ask_for_confirmation=False):
-        acl_value = {} if TestToolkit.tested_api == ApiType.OPENAPI else ""
-        result_obj = BaseComponent.set(self, op_param_name=acl_id, op_param_value=acl_value, expected_str=expected_str,
-                                       apply=apply, ask_for_confirmation=ask_for_confirmation)
-        if result_obj.result:
-            acl_id_obj = AclID(acl_id=acl_id, parent_obj=self)
-            self.acl_dict.update({acl_id: acl_id_obj})
-        return result_obj
-
-    def unset(self, acl_id="", expected_str="", apply=False, ask_for_confirmation=False):
-        if acl_id:
-            result_obj = BaseComponent.unset(self.acl_dict[acl_id], expected_str=expected_str, apply=apply,
-                                             ask_for_confirmation=ask_for_confirmation)
-            if result_obj.result:
-                self.acl_dict.pop(acl_id)
-        else:
-            result_obj = BaseComponent.unset(self, expected_str=expected_str, apply=apply, ask_for_confirmation=ask_for_confirmation)
-            if result_obj.result:
-                self.acl_dict = {}
-        return result_obj
+        super().__init__(parent=parent_obj, api={ApiType.NVUE: NvueBaseCli, ApiType.OPENAPI: OpenApiBaseCli}, path='/acl')
+        self.acl_id: Dict[str, AclID] = DefaultDict(lambda acl_id: AclID(parent_obj=self, acl_id=acl_id))
 
 
 class AclID(BaseComponent):
 
     def __init__(self, acl_id, parent_obj=None):
-        BaseComponent.__init__(self, parent=parent_obj, path=f'/{acl_id}')
+        super().__init__(parent=parent_obj, path=f'/{acl_id}')
         self.rule = Rule(self)
         self.inbound = BaseComponent(self, path='/inbound')
         self.outbound = BaseComponent(self, path='/outbound')
--- ngts/nvos_tools/acl/rule.py
+++ ngts/nvos_tools/acl/rule.py
@@ -1,9 +1,10 @@
 import logging
+from typing import Dict
 from ngts.nvos_tools.infra.BaseComponent import BaseComponent
-from ngts.nvos_tools.infra.NvosTestToolkit import TestToolkit
 from ngts.nvos_tools.acl.match import Match
 from ngts.nvos_tools.acl.action import Action
-from ngts.nvos_constants.constants_nvos import ApiType, AclConsts
+from ngts.nvos_constants.constants_nvos import AclConsts
+from ngts.nvos_tools.infra.DefaultDict import DefaultDict
 
 
 logger = logging.getLogger()
@@ -12,36 +13,14 @@ logger = logging.getLogger()
 class Rule(BaseComponent):
 
     def __init__(self, parent_obj=None):
-        BaseComponent.__init__(self, parent=parent_obj, path='/rule')
-        self.rules_dict = {}
-
-    def set(self, rule_id, expected_str='', apply=False, ask_for_confirmation=False):
-        rule_value = {} if TestToolkit.tested_api == ApiType.OPENAPI else ""
-        result_obj = BaseComponent.set(self, op_param_name=rule_id, op_param_value=rule_value, expected_str=expected_str,
-                                       apply=apply, ask_for_confirmation=ask_for_confirmation)
-        if result_obj.result:
-            rule_id_obj = RuleID(rule_id=rule_id, parent_obj=self)
-            self.rules_dict.update({rule_id: rule_id_obj})
-        return result_obj
-
-    def unset(self, rule_id="", expected_str="", apply=False, ask_for_confirmation=False):
-        if rule_id:
-            result_obj = BaseComponent.unset(self.rules_dict[rule_id], expected_str=expected_str, apply=apply,
-                                             ask_for_confirmation=ask_for_confirmation)
-            if result_obj.result:
-                self.rules_dict.pop(rule_id)
-        else:
-            result_obj = BaseComponent.unset(self, expected_str=expected_str, apply=apply,
-                                             ask_for_confirmation=ask_for_confirmation)
-            if result_obj.result:
-                self.rules_dict = {}
-        return result_obj
+        super().__init__(parent=parent_obj, path='/rule')
+        self.rule_id: Dict[str, RuleID] = DefaultDict(lambda rule_id: RuleID(parent_obj=self, rule_id=rule_id))
 
 
 class RuleID(BaseComponent):
 
     def __init__(self, rule_id=None, parent_obj=None):
-        BaseComponent.__init__(self, parent=parent_obj, path=f'/{rule_id}')
+        super().__init__(parent=parent_obj, path=f'/{rule_id}')
         self.match = Match(self)
         self.action = Action(self)
         self.recent_list = RecentList(self)
