979ddfe6095660a39988e63490e6be627385a437
Cong Hou <congh@nvidia.com>
2024-03-04 20:50:13 +0800

    Acl test support for t1-28-lag
    Change-Id: Ide01d944f853830d700e6a7542d540f02a996f44

--- tests/acl/test_acl.py
+++ tests/acl/test_acl.py
@@ -372,7 +372,7 @@ def setup(duthosts, ptfhost, rand_selected_dut, rand_unselected_dut, tbinfo, ptf
                 acl_table_ports[''] += port
 
     if topo in ["t0", "m0_vlan", "m0_l3"] or tbinfo["topo"]["name"] in ("t1-lag", "t1-64-lag", "t1-64-lag-clet",
-                                                                        "t1-56-lag"):
+                                                                        "t1-56-lag", "t1-28-lag"):
         for k, v in list(port_channels.items()):
             acl_table_ports[v['namespace']].append(k)
             # In multi-asic we need config both in host and namespace.
