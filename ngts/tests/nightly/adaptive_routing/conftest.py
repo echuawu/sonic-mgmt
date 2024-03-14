4ff4440973c9e77d232fa2c44c22f16cf3d32fb5
Aharon Malkin <amalkin@nvidia.com>
2024-03-14 11:09:35 +0200

    AR Phase 2
    Details:
        1. Fixed test infra to support global link utilization.
        2. Added global link utilization test.
        3. Fixed bug in reboot test - RM #3779674
        4. Fixed Max ports test- RM #3779719
        5. Fix test ip and frr configuration to support fast/warm reboot.
        6. Fixed profile parameters to be compatible with SPC4.
        7. Added validation for sonic cli commands.
        8. Added negative check for global link utilization
        9. Added skip by chip type to run AR tests only on SPC4 and higher
        10. Added skip to warm reboot type due to RM #3821130
        11. Added different infra fixes to avoid stale configuration on the DUT.
        12. Changed the teardown method to "yield" to fix wrong teardown flow done with finalizer.
        13. Added helper functions to validate traffic sending and distribution.
    
    Change-Id: If82a5d0cd187f61eb37979ce59bf0da4fafccbb1

--- ngts/tests/nightly/adaptive_routing/conftest.py
+++ ngts/tests/nightly/adaptive_routing/conftest.py
@@ -10,7 +10,9 @@ from ngts.common.checkers import is_feature_installed
 from ngts.constants.constants import AppExtensionInstallationConstants
 from ngts.tests.nightly.adaptive_routing.constants import ArConsts
 from ngts.helpers.reboot_reload_helper import get_supported_reboot_reload_types_list
+from ngts.tests.conftest import get_dut_loopbacks
 from tests.common.plugins.allure_wrapper import allure_step_wrapper as allure
+from infra.tools.redmine.redmine_api import is_redmine_issue_active
 
 logger = logging.getLogger()
 ar_helper = ArHelper()
@@ -23,10 +25,10 @@ def is_ar_supported(chip_type, sonic_branch):
     :param chip_type: chip_type fixture
     :param sonic_branch: sonic_branch fixture
     """
-    if chip_type == 'SPC' or chip_type == 'SPC1':
-        pytest.skip('Spectrum-1,2 does not support Adaptive Routing feature')
+    if chip_type == 'SPC' or chip_type == 'SPC2' or chip_type == 'SPC3':
+        pytest.skip('Adaptive Routing feature is supported on SPC4 and higher chip types')
     if sonic_branch in ['201911', '202012', '202205', '202211']:
-        pytest.skip('Adaptive Routing feature supported at 202305 and higher')
+        pytest.skip('Adaptive Routing feature is supported at 202305 and higher')
 
 
 @pytest.fixture(scope='module')
@@ -60,14 +62,13 @@ def check_feature_status(cli_objects, is_ar_supported):
 
 
 @pytest.fixture(scope='class')
-def ar_base_config_default_vrf(topology_obj, cli_objects, interfaces, engines, request):
+def ar_base_config_default_vrf(topology_obj, cli_objects, interfaces, engines):
     """
     This method is to apply DUT basic configuration
     :param topology_obj: topology_obj fixture
     :param cli_objects: cli_objects fixture
     :param interfaces: interfaces fixture
     :param engines: interfaces fixture
-    :param request: pytest builtin
     :return: transceiver port
     """
     ecmp_tx_port_list = [interfaces.dut_hb_1, interfaces.dut_hb_2]
@@ -105,82 +106,108 @@ def ar_base_config_default_vrf(topology_obj, cli_objects, interfaces, engines, r
             'cleanup': ['configure terminal', 'no router bgp', 'exit', 'exit']}
     }
 
-    ar_helper.enable_doai_service(cli_objects, request)
-    ar_helper.enable_ar_function(cli_objects, request)
-    IpConfigTemplate.configuration(topology_obj, ip_config_dict, request)
-    ar_helper.add_dummy_interface_hb(cli_objects, request)
+    ar_helper.enable_doai_service(cli_objects)
+    ar_helper.enable_ar_function(cli_objects)
+    IpConfigTemplate.configuration(topology_obj, ip_config_dict)
+    ar_helper.add_dummy_interface_hb(cli_objects)
     logger.info('Cleanup frr config, in case there is useless bgp configuration exist')
     FrrConfigTemplate.cleanup(topology_obj, frr_config_dict)
-    FrrConfigTemplate.configuration(topology_obj, frr_config_dict, request)
+    FrrConfigTemplate.configuration(topology_obj, frr_config_dict)
 
     logger.info('Verify bgp neighbor  established')
     ar_helper.verify_bgp_neighbor(cli_objects)
     logger.info('Verify ecmp routes')
     ar_helper.verify_ecmp_route(cli_objects, interfaces)
-    tx_port = ar_helper.get_tx_port_in_ecmp(cli_objects, ecmp_tx_port_list, ArConsts.PACKET_NUM_20)
-    ar_helper.enable_ar_port(cli_objects, ecmp_tx_port_list, port_util_percent=ArConsts.PORT_UTIL_DEFAULT_PERCENT,
-                             request=request)
-    logger.info('Copy and load AR custom profile')
-    ar_profile_config_folder_path = os.path.dirname(os.path.abspath(__file__)) + '/' + ArConsts.AR_CONFIG_FOLDER
-    ar_helper.copy_and_load_profile_config(engines, cli_objects, ar_profile_config_folder_path)
+    tx_port = ar_helper.get_tx_port_in_ecmp(cli_objects, ecmp_tx_port_list, ArConsts.PACKET_NUM_SMALLEST)
+    ar_helper.enable_ar_port(cli_objects, ecmp_tx_port_list, restart_swss=True)
     logger.info('Config save and reload, then check dockers and ports status')
     ar_helper.config_save_reload(cli_objects, topology_obj, reload_force=True)
     ar_helper.verify_bgp_neighbor(cli_objects)
-    return tx_port
+
+    yield tx_port
+
+    ar_helper.disable_ar_port(cli_objects, ecmp_tx_port_list)
+    FrrConfigTemplate.cleanup(topology_obj, frr_config_dict)
+    ar_helper.del_dummy_interface_hb(cli_objects, ArConsts.DUMMY_INTF['name'])
+    IpConfigTemplate.cleanup(topology_obj, ip_config_dict)
+    ar_helper.disable_ar(cli_objects)
+    ar_helper.disable_doai_service(cli_objects)
 
 
 @pytest.fixture(scope='class')
-def ar_max_ports_l3_config(topology_obj, cli_objects, interfaces, engines, request):
+def ar_max_ports_config(topology_obj, cli_objects, interfaces, engines):
     """
     This method is to apply L3 config for all DUT ports
     :param topology_obj: topology_obj fixture
     :param cli_objects: cli_objects fixture
     :param interfaces: interfaces fixture
     :param engines: interfaces fixture
-    :param request: pytest builtin
     """
-    ar_helper.enable_doai_service(cli_objects, request)
-    ar_helper.enable_ar_function(cli_objects, request)
-    port_list = ar_helper.get_all_ports(topology_obj)
+    ar_helper.enable_doai_service(cli_objects)
+    ar_helper.enable_ar_function(cli_objects)
+    ports_list = ar_helper.get_all_ports(topology_obj)
     dut_ports_config = []
-    for number, iface_name in enumerate(port_list):
-        dut_ports_config.append({'iface': iface_name, 'ips': [(f"10.10.10.{number}", '24')]})
+    for number, iface_name in enumerate(ports_list):
+        dut_ports_config.append({'iface': iface_name, 'ips': [(f"10.10.{number}.1", '24')]})
     ip_config_dict = {
         'dut': dut_ports_config
     }
-    IpConfigTemplate.configuration(topology_obj, ip_config_dict, request)
+    IpConfigTemplate.configuration(topology_obj, ip_config_dict)
+    ar_helper.enable_ar_port(cli_objects, ports_list)
+    cli_objects.dut.general.save_configuration()
+
+    yield ports_list
+
+    IpConfigTemplate.cleanup(topology_obj, ip_config_dict)
+    ar_helper.disable_ar_port(cli_objects, ports_list)
+    ar_helper.disable_ar(cli_objects)
+    ar_helper.disable_doai_service(cli_objects)
+    ar_helper.config_save_reload(cli_objects, topology_obj, reload_force=True)
 
 
 @pytest.fixture(scope='function')
-def config_ecmp_ports_speed_as_10G(interfaces, cli_objects, request):
+def config_ecmp_ports_speed_as_10G(interfaces, cli_objects):
     """
     This method is to configure ecmp ports speed to 10G
     :param interfaces: interfaces fixture
     :param cli_objects: cli_objects fixture
-    :param request: pytest builtin
     """
-    ar_helper.config_ecmp_ports_speed_to_10G(cli_objects, interfaces, request)
+    logger.info(f'Get original port speed of {interfaces.dut_hb_1, interfaces.dut_hb_2}')
+    original_intf_speeds = cli_objects.dut.interface.get_interfaces_speed(
+        [interfaces.dut_hb_1, interfaces.dut_hb_2])
+    ar_helper.config_ecmp_ports_speed_to_10G(cli_objects, interfaces)
+
+    yield
+
+    ar_helper.restore_ecmp_ports_speed(cli_objects, original_intf_speeds)
 
 
 @pytest.fixture(scope='function')
-def config_vlan_intf(cli_objects, request):
+def config_vlan_intf(cli_objects):
     """
     This method is to configure vlan interface
     :param cli_objects: cli_objects fixture
-    :param request: pytest builtin
     """
-    ar_helper.add_dummy_vlan_intf(cli_objects, request)
+    ar_helper.add_dummy_vlan_intf(cli_objects)
+
+    yield
+
+    ar_helper.del_dummy_vlan_intf(cli_objects)
 
 
 @pytest.fixture(scope='function')
-def config_lag(cli_objects, topology_obj, request):
+def config_lag(cli_objects, topology_obj):
     """
     This method is to configure LAG interface
     :param cli_objects: cli_objects fixture
     :param topology_obj: topology_obj fixture
-    :param request: pytest builtin
     """
-    ar_helper.add_lacp_intf(cli_objects, topology_obj, request)
+    lb = get_dut_loopbacks(topology_obj)
+    ar_helper.add_lacp_intf(cli_objects, lb)
+
+    yield
+
+    ar_helper.del_lacp_intf(cli_objects, lb[0][0])
 
 
 @pytest.fixture(scope='function')
@@ -192,6 +219,7 @@ def configure_port_shaper(cli_objects, interfaces, engines, topology_obj):
     :param engines fixture
     :param topology_obj: topology_obj fixture
     """
+    cli_objects.dut.general.save_configuration()
     logger.info('Copy actual config_db.json to DUT home dir')
     engines.dut.run_cmd(f'sudo cp {ArConsts.CONFIG_DB_FILE_PATH}{ArConsts.CONFIG_DB_FILE_NAME} '
                         f'{ArConsts.DUT_HOME_DIR}')
@@ -207,9 +235,10 @@ def configure_port_shaper(cli_objects, interfaces, engines, topology_obj):
 
     yield
 
-    with allure.step("Delete configured qos map and port scheduler"):
-        cli_objects.dut.interface.del_port_qos_map(interfaces.dut_hb_1, port_scheduler)
-        cli_objects.dut.interface.del_port_scheduler(port_scheduler)
+    logger.info('Enable back packet aging')
+    ar_helper.enable_packet_aging(engines)
+    logger.info('Configure port scheduler to default params')
+    cli_objects.dut.interface.config_port_scheduler(port_scheduler, 0)
 
     logger.info(f'Move file {ArConsts.CONFIG_DB_FILE_NAME} from {ArConsts.DUT_HOME_DIR} to '
                 f'{ArConsts.CONFIG_DB_FILE_PATH}')
@@ -226,24 +255,49 @@ def get_reboot_type(platform_params, is_simx):
     :return: reboot type
     """
     supported_reboot_reload_list = get_supported_reboot_reload_types_list(platform_params.platform)
+
+    # TODO- remove WA after bug is solved
+    if "warm-reboot" in supported_reboot_reload_list and is_redmine_issue_active([3821130]):
+        supported_reboot_reload_list.remove("warm-reboot")
+
     reboot_type = random.choice(supported_reboot_reload_list)
     if is_simx:
         reboot_type = random.choice(ArConsts.SIMX_REBOOT_TYPES)
     return reboot_type
 
 
-@pytest.fixture(scope='class')
-def ar_config_for_max_port(cli_objects, topology_obj, request):
-    """
-    This fixture is to configure AR at all DUT ports
-    :param cli_objects: cli_objects fixture
-    :param topology_obj: topology_obj fixture
-    :param request: pytest builtin
-    return: ports list
-    """
-    # Get available port list
-    ports_list = ar_helper.get_all_ports(topology_obj)
-    # Configure AR at port
-    ar_helper.enable_ar_port(cli_objects, ports_list, port_util_percent=ArConsts.PORT_UTIL_DEFAULT_PERCENT,
-                             request=request)
-    return ports_list
+@pytest.fixture(scope='function')
+def configure_global_util(cli_objects):
+    # Configure AR port with low link utilization rate
+    with allure.step(f'Configure link utilization grade to {ArConsts.PORT_UTIL_CUSTOM_PERCENT}%'):
+        ar_helper.enable_global_link_utilization(cli_objects, threshold=ArConsts.PORT_UTIL_CUSTOM_PERCENT, restart_swss=True)
+        cli_objects.dut.general.verify_dockers_are_up()
+    # Wait for bgp neighbor come up
+    ar_helper.verify_bgp_neighbor(cli_objects)
+    ar_helper.add_routes_to_host(cli_objects)
+
+    yield
+
+    # Return link utilization to previous state
+    ar_helper.disable_link_utilization(cli_objects, restart_swss=True)
+    ar_helper.del_routes_from_host(cli_objects)
+
+
+@pytest.fixture(scope='function')
+def config_ar_profile(cli_objects, engines, topology_obj):
+    # Copy AR custom profile to dut
+    logger.info('Copy and load AR custom profile')
+    ar_profile_config_folder_path = os.path.dirname(os.path.abspath(__file__)) + '/' + ArConsts.AR_CONFIG_FOLDER
+    ar_helper.copy_and_load_profile_config(engines, cli_objects, ar_profile_config_folder_path)
+    # Enable AR custom profile
+    with allure.step(f'Enable {ArConsts.CUSTOM_PROFILE_NAME} profile'):
+        ar_helper.enable_ar_profile(cli_objects, ArConsts.CUSTOM_PROFILE_NAME)
+        ar_helper.config_save_reload(cli_objects, topology_obj, reload_force=True)
+        ar_helper.verify_bgp_neighbor(cli_objects)
+
+    yield
+
+    with allure.step(f'Return to default profile {ArConsts.GOLDEN_PROFILE0} at the end of the test'):
+        ar_helper.enable_ar_profile(cli_objects, ArConsts.GOLDEN_PROFILE0)
+        ar_helper.config_save_reload(cli_objects, topology_obj, reload_force=True)
+        ar_helper.verify_bgp_neighbor(cli_objects)
