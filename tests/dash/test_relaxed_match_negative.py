b2a8e3825034f79d8d5696b5944353588d174df8
Cong Hou <congh@nvidia.com>
2024-03-20 16:10:27 +0800

    Removed the invalid port test case
    This test is not supported, for detail, check RM#3753419.
    
    Change-Id: I22691f1d3e81169e2e964a8c9453cf14a37f74ef

--- tests/dash/test_relaxed_match_negative.py
+++ tests/dash/test_relaxed_match_negative.py
@@ -1,12 +1,10 @@
 import logging
 import pytest
 import ptf.testutils as testutils
-import time
 
 from constants import LOCAL_PTF_INTF, REMOTE_PTF_INTF
 from tests.common.plugins.allure_wrapper import allure_step_wrapper as allure
 from tests.dash.conftest import config_vxlan_udp_dport
-from tests.common.helpers.assertions import pytest_assert
 import packets
 
 logger = logging.getLogger(__name__)
@@ -52,24 +50,3 @@ def test_relaxed_match_negative(duthost, ptfadapter, apply_vnet_configs, dash_co
         _, vxlan_packet, expected_packet = packets.outbound_vnet_packets(dash_config_info, vxlan_udp_dport=13330)
         testutils.send(ptfadapter, dash_config_info[LOCAL_PTF_INTF], vxlan_packet, 1)
         testutils.verify_no_packet_any(ptfadapter, expected_packet, ports=dash_config_info[REMOTE_PTF_INTF])
-
-
-def test_relaxed_match_invalid_port(duthost, ptfadapter, apply_vnet_configs, dash_config_info, acl_default_rule):
-    """
-    Negative test of invalid UDP port
-    """
-    invalid_port_list = ['', 'string', 65536]
-    for port in invalid_port_list:
-        with allure.step(f"Configure the invalid port {port} to DPU"):
-            config_vxlan_udp_dport(duthost, port)
-    with allure.step("Wait 10 seconds and check all critical services are running"):
-        CHECK_INTERVAL = 5
-        CHECK_TIMES = 12
-        for i in range(CHECK_TIMES):
-            pytest_assert(duthost.critical_services_fully_started(),
-                          "Some critical services are not running, please check the test log.")
-            time.sleep(CHECK_INTERVAL)
-    with allure.step("Check the actual port is still 4789"):
-        _, vxlan_packet, expected_packet = packets.outbound_vnet_packets(dash_config_info)
-        testutils.send(ptfadapter, dash_config_info[LOCAL_PTF_INTF], vxlan_packet, 1)
-        testutils.verify_packets_any(ptfadapter, expected_packet, ports=dash_config_info[REMOTE_PTF_INTF])
