65287631558c5c24b80e76f5250e830a4e5a6fe0
Sharon Lutati <slutati@nvidia.com>
2024-03-27 14:49:42 +0200

    Update ngts tag to 1.2.290
    Details:
    New ngts version include a privileged flag for simx containers bringup.
    This is needed for our simx setup to function properly.
    
    Change-Id: Ib0f9abd4d4a49de1ce373fdc1db6d600a06fc0ee

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -392,7 +392,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.288'}
+    default_list = {'docker-ngts': '1.2.290'}
     return default_list.get(docker_name, latest)
 
 
