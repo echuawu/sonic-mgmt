f01028445a8658a28e9750a4350333bbebe97f64
jbao <jbao@nvidia.com>
2024-04-16 06:47:06 +0300

    Fix review comments for test_dpu_npu_port
    Change-Id: I6f23e1ef43dff7892a8bd591970af64b5917bc89

--- tests/platform_tests/test_dpu_npu_port.py
+++ tests/platform_tests/test_dpu_npu_port.py
@@ -1,28 +1,40 @@
-import logging
 import pytest
 from tests.common.helpers.assertions import pytest_assert
+from tests.common.plugins.allure_wrapper import allure_step_wrapper as allure
 
 pytestmark = [
     pytest.mark.topology('any')
 ]
 
+sku_dpu_pdu_ports_map = {"Mellanox-SN4700-O28": ["Ethernet224", "Ethernet232", "Ethernet240", "Ethernet248"],
+                         "Nvidia-9009d3b600CVAA-C1": ["Ethernet0"]}
 
-def test_dpu_npu_port(duthosts, enum_rand_one_per_hwsku_frontend_hostname, dpu_npu_port_list):
+
+def test_dpu_npu_port(duthosts, rand_one_dut_hostname, dpu_npu_port_list):
     """
-    This test case is to verify when the role of port is Dpc, the type of port is  DPU-NPU Data Port
+    This test case is to verify
+    1. The relevant sku include the corresponding dpu npu ports
+    2. When the role of port is Dpc, the type of port is  DPU-NPU Data Port
     """
-    duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+    duthost = duthosts[rand_one_dut_hostname]
     intf_status = duthost.show_interface(command="status")["ansible_facts"]['int_status']
     dpu_npu_port_type = 'DPU-NPU Data Port'
+    dut_hwsku = duthost.facts["hwsku"]
+    dpu_npu_ports = dpu_npu_port_list[duthost.hostname]
 
-    logging.info("Check interface type for dpu npu port")
-    if not dpu_npu_port_list[duthost.hostname]:
+    if dut_hwsku in sku_dpu_pdu_ports_map:
+        with allure.step(f"check sku {dut_hwsku} include dpu npu ports :{sku_dpu_pdu_ports_map[dut_hwsku]}"):
+            pytest_assert(set(dpu_npu_ports) == set(sku_dpu_pdu_ports_map[dut_hwsku]),
+                          f"For {dut_hwsku}, the expected ports are:{sku_dpu_pdu_ports_map[dut_hwsku]},"
+                          f"the actual dpu npu port are {dpu_npu_ports}",)
+    elif not dpu_npu_ports:
         pytest.skip("Skip the test due to no dpu npu ports")
 
-    for intf in dpu_npu_port_list[duthost.hostname]:
-        pytest_assert(
-            intf in intf_status, f"interface {intf} is not in the output of show interface status: {intf_status}")
-        pytest_assert(
-            intf_status[intf].get('type') == dpu_npu_port_type,
-            f"Interface {intf}: The actual type is is {intf_status[intf].get('type')}, "
-            f"the expected type is {dpu_npu_port_type}")
+    with allure.step("Check interface type for dpu npu port"):
+        for intf in dpu_npu_ports:
+            pytest_assert(
+                intf in intf_status, f"interface {intf} is not in the output of show interface status: {intf_status}")
+            pytest_assert(
+                intf_status[intf].get('type') == dpu_npu_port_type,
+                f"Interface {intf}: The actual type is is {intf_status[intf].get('type')}, "
+                f"the expected type is {dpu_npu_port_type}")
