04297f79f44c2798dcdc4fa375075c60dba4ce6e
Illia Kotvitskyi <ikotvitskyi@nvidia.com>
2024-04-22 17:58:17 +0300

    adjust Tune config files for Python3
    Change-Id: Iddc773cca60cd82fe14ccb8b3025e78b7ff2bf05

--- sonic-tool/mars/scripts/lib/setup_env.py
+++ sonic-tool/mars/scripts/lib/setup_env.py
@@ -1,10 +1,11 @@
+import site
 import sys
 from lib import constants
 
 
 def extend_sys_path():
     if constants.VER_SDK_PATH not in sys.path:
-        sys.path.append(constants.VER_SDK_PATH)
+        site.addsitedir(constants.VER_SDK_PATH)
     for p in constants.EXTRA_PACKAGE_PATH_LIST:
         if p not in sys.path:
             sys.path.append(p)
