c49b40632d4922f4f961c7af8b24f964e3236698
jbao <jbao@nvidia.com>
2024-04-23 09:07:38 +0300

    Fix test_dpu_npu_port comment
    Remove sku_dpu_pdu_ports_map, instead get the dpu npu ports from hwsku.json
    
    Change-Id: I459a3d035c2b94914dde808963363ec77030bf19

--- tests/platform_tests/test_dpu_npu_port.py
+++ tests/platform_tests/test_dpu_npu_port.py
@@ -1,19 +1,17 @@
 import pytest
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.plugins.allure_wrapper import allure_step_wrapper as allure
+from tests.common.platform.interface_utils import get_dpu_npu_ports_from_hwsku
 
 pytestmark = [
     pytest.mark.topology('any')
 ]
 
-sku_dpu_pdu_ports_map = {"Mellanox-SN4700-O28": ["Ethernet224", "Ethernet232", "Ethernet240", "Ethernet248"],
-                         "Nvidia-9009d3b600CVAA-C1": ["Ethernet0"]}
-
 
 def test_dpu_npu_port(duthosts, rand_one_dut_hostname, dpu_npu_port_list):
     """
     This test case is to verify
-    1. The relevant sku include the corresponding dpu npu ports
+    1. The dpu npu ports defined in hwsku.json are same as that in config db
     2. When the role of port is Dpc, the type of port is  DPU-NPU Data Port
     """
     duthost = duthosts[rand_one_dut_hostname]
@@ -21,13 +19,14 @@ def test_dpu_npu_port(duthosts, rand_one_dut_hostname, dpu_npu_port_list):
     dpu_npu_port_type = 'DPU-NPU Data Port'
     dut_hwsku = duthost.facts["hwsku"]
     dpu_npu_ports = dpu_npu_port_list[duthost.hostname]
+    dpu_npu_port_list_in_hwsku = get_dpu_npu_ports_from_hwsku(duthost)
 
-    if dut_hwsku in sku_dpu_pdu_ports_map:
-        with allure.step(f"check sku {dut_hwsku} include dpu npu ports :{sku_dpu_pdu_ports_map[dut_hwsku]}"):
-            pytest_assert(set(dpu_npu_ports) == set(sku_dpu_pdu_ports_map[dut_hwsku]),
-                          f"For {dut_hwsku}, the expected ports are:{sku_dpu_pdu_ports_map[dut_hwsku]},"
-                          f"the actual dpu npu port are {dpu_npu_ports}",)
-    elif not dpu_npu_ports:
+    if dpu_npu_port_list_in_hwsku:
+        with allure.step(f"check dpu port in config db are same as the dpu ports in hwsku"):
+            pytest_assert(set(dpu_npu_ports) == set(dpu_npu_port_list_in_hwsku),
+                          f"For {dut_hwsku}, the dpu npu ports in hwsku.json are:{dpu_npu_port_list_in_hwsku},"
+                          f"the dpu npu ports in config db are {dpu_npu_ports}",)
+    else:
         pytest.skip("Skip the test due to no dpu npu ports")
 
     with allure.step("Check interface type for dpu npu port"):
