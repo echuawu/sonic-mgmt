4a7daa4dd0568eb49bf701162a11c399312d8a51
Cong Hou <congh@nvidia.com>
2024-04-24 11:44:06 +0800

    update the dpu ip assignment test for Bobcat
    On bobcat, the configuration is generated directly from minigraph. And vlan 4094 is not required, the IP addresses are different.
    
    Change-Id: Ia45554d8fa79d9271687baf20b2c77ccfe62880a

--- tests/virtual_smart_switch/test_dpu_ip_assignment.py
+++ tests/virtual_smart_switch/test_dpu_ip_assignment.py
@@ -1,14 +1,14 @@
 import pytest
 import logging
-import os
 import re
 from tests.common.plugins.allure_wrapper import allure_step_wrapper as allure
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.config_reload import config_reload
 from tests.common.utilities import wait_until
+from tests.common.platform.interface_utils import get_dpu_npu_ports_from_hwsku
 
-IP_ADDRESS_LIST = ["10.255.255.1", "10.255.255.2", "10.255.255.3", "10.255.255.4"]
-INTERNAL_PORTS = {"Mellanox-SN4700-O28": ["Ethernet224", "Ethernet232", "Ethernet240", "Ethernet248"]}
+IP_ADDRESS_LIST = {"Mellanox-SN4700-O28": ["10.255.255.1", "10.255.255.2", "10.255.255.3", "10.255.255.4"],
+                   "ACS-SN4280": ["169.254.200.1", "169.254.200.2", "169.254.200.3", "169.254.200.4"]}
 
 logger = logging.getLogger(__name__)
 
@@ -26,45 +26,36 @@ def skip_non_smartswitch_testbed(duthost, tbinfo):
 
 @pytest.fixture(autouse=True)
 def apply_ip_assignment_config(duthost):
-    # Apply the ip assignment config if it was not applied in deployment
-    config_applied_by_test = False
-    config_facts = duthost.get_running_config_facts()
-    if not config_facts.get('VLAN'):
-        with allure.step('Apply virtual smart switch configuration'):
-            duthost.copy(src='virtual_smart_switch/dpu_ip_assignment_config.json',
-                         dest='/tmp/dpu_ip_assignment_config.json')
-            duthost.shell('sudo sonic-cfggen -j /tmp/dpu_ip_assignment_config.json --write-to-db')
-            duthost.shell('sudo config save -y')
-            config_applied_by_test = True
-            config_reload(duthost, safe_reload=True)
+    # This is only for the virtual smart switch, the real one does not need this
+    if "4700" in duthost.facts["hwsku"]:
+        # Apply the ip assignment config if it was not applied in deployment
+        config_applied_by_test = False
+        config_facts = duthost.get_running_config_facts()
+        if not config_facts.get('VLAN'):
+            with allure.step('Apply virtual smart switch configuration'):
+                duthost.copy(src='virtual_smart_switch/dpu_ip_assignment_config.json',
+                             dest='/tmp/dpu_ip_assignment_config.json')
+                duthost.shell('sudo sonic-cfggen -j /tmp/dpu_ip_assignment_config.json --write-to-db')
+                duthost.shell('sudo config save -y')
+                config_applied_by_test = True
+                config_reload(duthost, safe_reload=True)
 
     yield
 
-    if config_applied_by_test:
-        with allure.step('Restore the config via loading minigraph'):
-            config_reload(duthost, config_source='minigraph', safe_reload=True, check_intf_up_ports=True)
+    if "4700" in duthost.facts["hwsku"]:
+        if config_applied_by_test:
+            with allure.step('Restore the config via loading minigraph'):
+                config_reload(duthost, config_source='minigraph', safe_reload=True, check_intf_up_ports=True)
 
 
 def test_dpu_ip_assignment(duthost):
     hwsku = duthost.facts["hwsku"]
-    internal_port_list = INTERNAL_PORTS.get(hwsku, INTERNAL_PORTS["Mellanox-SN4700-O28"])
+    internal_port_list = get_dpu_npu_ports_from_hwsku(duthost)
     with allure.step("Check the internal port status are up"):
         pytest_assert(duthost.links_status_up(internal_port_list), "Not all internal ports are up")
-    with allure.step("Check the vlan interface is up and IP address is correct"):
-        output = duthost.shell("show ip interface | grep Vlan4094")['stdout']
-        pattern = r"10\.255\.255\.254.*up/up"
-        pytest_assert(re.search(pattern, output),
-                      "The vlan interface is not up or the IP address is not correct.")
-    with allure.step("Check the vlan 4094 contains the internal ports"):
-        output = duthost.shell("show vlan brief")['stdout']
-        for port in internal_port_list:
-            pytest_assert(re.search(port, output), f"The port {port} is not the member of vlan 4094")
-    with allure.step("Check the DHCP server status"):
-        output = duthost.shell("show dhcp_server ipv4 info")['stdout']
-        pattern = r"Vlan4094.*PORT.*10\.255\.255\.254.*enabled"
-        pytest_assert(re.search(pattern, output), "The DHCP server info is not correct.")
     with allure.step("Check the DHCP lease status"):
         def check_dhcp_lease():
+            duthost.shell("show dhcp_server ipv4 info")
             output = duthost.shell("show dhcp_server ipv4 lease")['stdout']
             for port in internal_port_list:
                 if not re.search(port, output):
@@ -72,6 +63,6 @@ def test_dpu_ip_assignment(duthost):
             return True
         pytest_assert(wait_until(60, 5, 0, check_dhcp_lease), f"There is no lease for all internal ports, "
                                                               f"please check the test log.")
-    for address in IP_ADDRESS_LIST:
+    for address in IP_ADDRESS_LIST.get(hwsku, IP_ADDRESS_LIST["ACS-SN4280"]):
         with allure.step(f"Ping the dpu IP address {address}"):
             duthost.shell(f"ping -c 5 {address}")
