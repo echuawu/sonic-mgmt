60484af246c5d5cf3295bd48d18fa72423c11d54
echuawu <chuanw@nvidia.com>
2024-04-30 12:33:10 +0800

    Update script test_verify_fanout_version to support sonic fanout
    Update script test_verify_fanout_version to support sonic fanout
    
    Change-Id: Ifffa009eda171d7844d796a7fd7bb4fbcec62941

--- ngts/scripts/test_verify_fanout_version.py
+++ ngts/scripts/test_verify_fanout_version.py
@@ -1,7 +1,9 @@
 import logging
 import pytest
+from ngts.constants.constants import CliType
 
-EXPECTED_VERSION = "3.10.4206"
+EXPECTED_MLNX_VERSION = "3.10.4206"
+EXPECTED_SONIC_VERSION = "202311_RC.36-b4a12122b_Internal"
 
 logger = logging.getLogger()
 
@@ -13,5 +15,14 @@ def test_verify_fanout_version(topology_obj):
     """
     logger.info("Show version concise on the fanout")
     fanout_engine = topology_obj.players['fanout']['engine']
-    current_version = fanout_engine.run_cmd('show version concise')
-    assert EXPECTED_VERSION in current_version
+    fanout_engine_type = topology_obj.players['fanout']['attributes'].noga_query_data['attributes']['Topology Conn.'][
+        'CLI_TYPE']
+
+    if fanout_engine_type == CliType.SONIC:
+        logger.info("This is a SONiC fanout switch")
+        current_version = fanout_engine.run_cmd('show version')
+        assert EXPECTED_SONIC_VERSION in current_version
+    else:
+        logger.info("This is an ONYX fanout switch")
+        current_version = fanout_engine.run_cmd('show version concise')
+        assert EXPECTED_MLNX_VERSION in current_version
