344b8ab2a066e08013f917f6e7f590b7a5539074
jbao <jbao@nvidia.com>
2024-05-31 13:11:50 +0300

    Add test for read eeprom and write eeprom
    HLD:https://github.com/sonic-net/SONiC/blob/master/doc/sfputil/read_write_eeprom_by_page.md
    
    Change-Id: I4fc23566a6b43eb0107e06a18d72dfcef977c154
    (cherry picked from commit d5093ab0c63d6c5adc757bac4d0ca5be246cc9f1)

--- tests/platform_tests/sfp/im/test_im.py
+++ tests/platform_tests/sfp/im/test_im.py
@@ -1,7 +1,12 @@
 import pytest
+import logging
+import random
 
 from tests.platform_tests.sfp.im.helpers import *
 from tests.common.config_reload import config_reload
+from tests.platform_tests.sfp.util import get_sfp_type, get_dev_conn, read_eeprom_by_page_and_byte,\
+    write_eeprom_by_page_and_byte, DICT_WRITABLE_BYTE_FOR_PAGE_0
+from tests.common.plugins.allure_wrapper import allure_step_wrapper as allure
 
 cmd_interface_transceiver = "show interface transceiver eeprom"
 cmd_sfputil_eeprom = "sudo sfputil show eeprom"
@@ -12,6 +17,8 @@ cmd_redis_tranceiver_status = 'redis-cli -n 6 hgetall "TRANSCEIVER_STATUS|{}"'
 pytestmark = [
     pytest.mark.topology('any')
 ]
+
+logger = logging.getLogger()
 
 
 class TestIndependentModuleFunctional:
@@ -80,3 +87,73 @@ class TestIndependentModuleFunctional:
             mlxlink_output = get_mlxlink_ber(self.duthost, port)
             assert int(mlxlink_output[BER_EFFECTIVE_PHYSICAL_ERRORS]) == 0, f"{BER_EFFECTIVE_PHYSICAL_ERRORS} > 0 "
             assert mlxlink_output[BER_EFFECTIVE_PHYSICAL_BER] == '15E-255', f"{BER_EFFECTIVE_PHYSICAL_BER} > 15E-255"
+
+    def test_read_write_eeprom_by_page_and_byte(self, enum_rand_one_per_hwsku_frontend_hostname,
+                                                      enum_frontend_asic_index, conn_graph_facts, xcvr_skip_list):
+        """
+        This test is verify read and write eeprom by page and byte.
+        1. Get all sfp type of all sfp types by reading the first byte of 0 page
+        2. Verify write eeprom function for ports supporting FW module management
+          2.1. Write the writable byte, verify there is no any error
+          2.2  Write the writable byte with verify option, verify there is no any error,
+               and verify the read value is equal to the written value
+          2.4  Write the read-only byte with verify option, verify "Error: Write data failed!" is in the output
+        3. Recover the written byte with the original value fot all tested ports
+        """
+
+        portmap, dev_conn = get_dev_conn(self.duthost, conn_graph_facts, enum_frontend_asic_index)
+        sfp_type_im_port_dict = {}
+
+        with allure.step(f"Get sfp type by reading the first byte of 0 page in eeprom"):
+            for intf in dev_conn:
+                if intf not in xcvr_skip_list[self.duthost.hostname]:
+                    sfp_type = get_sfp_type(self.duthost, intf)
+                    assert sfp_type,  f"Failed to get sfp type {sfp_type} for port {intf}"
+                    if intf in self.im_port_list:
+                        sfp_type_im_port_dict.update({intf: sfp_type})
+
+        original_port_to_eeprom_dict = {}
+        try:
+            with allure.step(f"Verify Writing eeprom for {self.im_port_list}"):
+                for intf in self.im_port_list:
+                    page = 0
+                    offset = DICT_WRITABLE_BYTE_FOR_PAGE_0[sfp_type_im_port_dict[intf]]
+                    data = "15"
+
+                    original_eeprom = read_eeprom_by_page_and_byte(self.duthost, intf, sfp_type, page, offset)
+                    original_port_to_eeprom_dict.update({intf: [offset, original_eeprom]})
+
+                    with allure.step(f"Verify writing writable byte {offset} for port {intf} with data {data} "):
+
+                        output_write_eeprom = write_eeprom_by_page_and_byte(
+                            self.duthost, intf, sfp_type, data, page, offset)
+                        assert not output_write_eeprom, \
+                            f"Failed to write eeprom for {intf}. output is: {output_write_eeprom}"
+
+                        output_write_eeprom = write_eeprom_by_page_and_byte(
+                            self.duthost, intf, sfp_type, data, page, offset, is_verify=True)
+                        assert not output_write_eeprom, \
+                            f"Failed to write eeprom for {intf} with verify option. output is {output_write_eeprom}"
+
+                        output_read_eeprom = read_eeprom_by_page_and_byte(self.duthost, intf, sfp_type, page, offset)
+
+                        assert output_read_eeprom == data, \
+                            "write data {data} doesn't match the read data {output_read_eeprom}"
+
+                    read_only_byte = 20
+                    with allure.step(
+                            f"Verify writing read-only byte {read_only_byte} for port {intf} with data {data}"):
+                        output_write_eeprom = write_eeprom_by_page_and_byte(
+                            self.duthost, intf, sfp_type, data, page, read_only_byte,
+                            is_verify=True, module_ignore_errors=True)
+                        assert "Error: Write data failed! " in output_write_eeprom, \
+                            f"Data should not be written to non-writable byte for {intf} " \
+                            f"for offset {read_only_byte}. output is {output_write_eeprom}"
+        except Exception as err:
+            logger.error(f"Failed to write eeprom: {err}")
+            raise AssertionError(err)
+        finally:
+            for intf, offset_data_info in original_port_to_eeprom_dict.items():
+                with allure.step(f"Recover original eeprom for {intf} with {offset_data_info}"):
+                    write_eeprom_by_page_and_byte(
+                        self.duthost, intf, sfp_type, offset_data_info[1], page, offset_data_info[0])
