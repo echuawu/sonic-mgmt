a1e31ea1600359487bc1160a88c3556560717957
Elias Abboud <eabboud@nvidia.com>
2024-06-24 15:28:11 +0300

    Test was calling wrong function name - fixed to sleep_after_system_reboot
    Change-Id: I82e4d3d8d5d126bad72dc511329b361a9ba54b43

--- ngts/tests_nvos/infra/init_flow/test_system_init_done_feature.py
+++ ngts/tests_nvos/infra/init_flow/test_system_init_done_feature.py
@@ -34,7 +34,7 @@ def test_system_ready_state_up(engines, devices, topology_obj):
                             should_wait_till_system_ready=False, confirm=True).verify_result()
 
     # TODO - WA once "checkpoint # " prints are removed and the reboot is faster.
-    devices.dut.sleep_after_reboot()
+    devices.dut.sleep_after_system_reboot()
 
     with allure.step('reconnect to the switch'):
         serial_engine = ConnectionTool.create_serial_connection(topology_obj, devices)
@@ -108,7 +108,7 @@ def test_system_ready_state_down(engines, devices, topology_obj):
         DutUtilsTool.reload(engine=engines.dut, device=devices.dut, command=reload_cmd_set, should_wait_till_system_ready=False, confirm=True).verify_result()
 
     # TODO - WA once "checkpoint # " prints are removed and the reboot is faster.
-    devices.dut.sleep_after_reboot()
+    devices.dut.sleep_after_system_reboot()
 
     with allure.step('reconnect to the switch'):
         serial_engine = ConnectionTool.create_serial_connection(topology_obj, devices)
