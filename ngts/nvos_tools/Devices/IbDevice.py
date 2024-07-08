f71103021fe5d6042e15acd3fc7d07068af5535d
Elias Abboud <eabboud@nvidia.com>
2024-07-08 17:19:48 +0300

    Adding first test case for NMX
    Change-Id: Ic63369c837e9770c894b4cdc7c8f73d3fd82e058

--- ngts/nvos_tools/Devices/IbDevice.py
+++ ngts/nvos_tools/Devices/IbDevice.py
@@ -673,6 +673,9 @@ class JulietScaleoutSwitch(JulietSwitch):
     def _init_constants(self):
         super()._init_constants()
         self.asic_type = NvosConst.NVL5
+        self.cluster_app_nmx_controller = {'app-id': 'nmx-c-nvos', 'app-ver': '0.4', 'capabilities': 'sm, gfm, fib, gw-api', 'components-ver': 'sm:5.19.0_ccc158a, gfm:560.00.03-bringup-j, fib-fe:0.4.1'}
+        self.cluster_app_nmx_telemetry = {'app-id': 'nmx-telemetry', 'app-ver': '0.4.1', 'capabilities': 'ib-telemetry', 'components-ver': 'nmx-telemetry:0.4.1, nmx-connector:0.4.1'}
+        self.cluster_app = {'nmx-controller': self.cluster_app_nmx_controller, 'nmx-telemetry': self.cluster_app_nmx_telemetry}
         self.reboot_type = 'julietscaleout_reboot'
         self.reset_factory = 'julietscaleout reset factory'
         self.generate_tech_support = 'julietscaleout generate_tech_support'
